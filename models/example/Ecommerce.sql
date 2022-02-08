cf484569e40d22ed388996b0d89d8c0924a69303
naveenpatnana <90193804+naveenpatnana@users.noreply.github.com>
2022-02-08 17:45:55 +0530

    Update Ecommerce.sql

--- models/example/Ecommerce.sql
+++ models/example/Ecommerce.sql
@@ -1,18 +1,15 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": 'DROP table  IF EXISTS "MM_TEST"."TEST"."NEWPERSONA";
-create table "MM_TEST"."TEST"."NEWPERSONA" (name varchar(25));',
+                                "sql": "DROP table  IF EXISTS \"MM_TEST\".\"TEST\".\"NEWPERSONA\";
+create table \"MM_TEST\".\"TEST\".\"NEWPERSONA\" (name varchar(25));",
                                 "transaction": true
                             }
                         ) }}
                         with sample_data as (
-
                             select * from MM_TEST.TEST.Ecommerce
                         ),
-                        
                         final as (
                             select * from sample_data
                         )
                         select * from final
-                        
