a5fb27e974f9c89ead4355d2100a99db3131232d
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-04-11 13:56:45 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "CREATE OR REPLACE TABLE Maplemonk.MAPLEMONK.MARKETING_CONSOLIDATED_FEMORA AS select Adset_Name,Adset_ID,Account_Name, Account_ID, Campaign_Name, Campaign_ID,Fb.date_start Date ,month(Fb.date_start) as month ,year(Fb.date_start) as year ,SUM(CLICKS) Clicks,SUM(SPEND) Spend,SUM(IMPRESSIONS) Impressions ,SUM(NVL(Fc.Conversions,0)) Conversions ,SUM(NVL(Fcv.Conversion_Value,0)) Conversion_Value ,\'Facebook Ads\' Channel from MAPLEMONK.MAPLEMONK.FACEBOOK_ADS_INSIGHTS Fb left join( select ad_id,date_start ,SUM(C.value:value) Conversions from MAPLEMONK.MAPLEMONK.FACEBOOK_ADS_INSIGHTS,lateral flatten(input => ACTIONS) C where C.value:action_type=\'offsite_conversion.fb_pixel_purchase\' group by ad_id,date_start having SUM(C.value:value) is not null)Fc ON Fb.ad_id = Fc.ad_id and Fb.date_start=Fc.date_start left join( select ad_id,date_start ,SUM(CV.value:value) Conversion_Value from MAPLEMONK.MAPLEMONK.FACEBOOK_ADS_INSIGHTS,lateral flatten(input => ACTION_VALUES) CV where CV.value:action_type=\'offsite_conversion.fb_pixel_purchase\' group by ad_id,date_start having SUM(CV.value:value) is not null)Fcv ON Fb.ad_id = Fcv.ad_id and Fb.date_start=Fcv.date_start group by Adset_Name, Adset_ID, Account_Name, Account_ID, Campaign_Name, Campaign_ID,Fb.date_start",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from MAPLEMONK.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
