9d92a69817ab8398240a2f4bbb20c62689832c1d
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-04-19 19:37:44 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "Create or replace table Almowear_DB.MAPLEMONK.Sales_Cost_Source as select date(FI.order_timestamp) Date ,FI.FINAL_UTM_CHANNEL Channel ,sum(FI.net_sales) Gross_Sales ,count(distinct FI.order_id) Orders ,count(distinct(case when lower(FI.customer_flag) = \'new\' then FI.order_id end)) as New_Customer_Orders ,count(distinct(case when lower(FI.customer_flag) = \'new\' then FI.customer_id end)) as New_Customers ,count(distinct(case when lower(FI.customer_flag) = \'repeated\' then FI.customer_id end)) as Repeat_Customers ,sum(FI.discount) DISCOUNT ,sum(case when lower(FI.customer_flag) = \'new\' then DISCOUNT end) as New_Customer_DISCOUNT ,sum(MC.spend) Marketing_Spend from fact_items FI left join Almowear_DB.MAPLEMONK.MARKETING_CONSOLIDATED_AW MC on date(FI.order_timestamp) = MC.date and FI.FINAL_UTM_CHANNEL = MC.channel where FI.source not in (\'Amazon\') group by 1,2 order by date desc",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from ALMOWEAR_DB.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
