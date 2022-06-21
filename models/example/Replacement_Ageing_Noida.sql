a4d97ef96a1463ff3b97e117f35eaab9d1f16977
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-06-21 12:33:19 +0000

    model file has updated

---
+++ models/example/Replacement_Ageing_Noida.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "ALTER TABLE eggozdb.maplemonk.replacement_ageing_noida ADD (Ddate Date); UPDATE eggozdb.maplemonk.replacement_ageing_noida SET Ddate = TRY_TO_DATE(\"Receiving Date\",\'DD/MM/YYYY\');",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from EGGOZDB.MAPLEMONK.Replacement_Ageing_Noida
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
