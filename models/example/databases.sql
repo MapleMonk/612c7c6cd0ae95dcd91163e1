4b8ba20e13210a952a46250d567be52ae3944bca
naveenpatnana <naveen_patnana@maplemonk.com>
2022-07-01 17:37:58 +0530

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "CREATE TABLE DEPARTMENT( ID INT PRIMARY KEY NOT NULL, DEPT CHAR(50) NOT NULL, EMP_ID INT NOT NULL );",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from pg_table_def
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
