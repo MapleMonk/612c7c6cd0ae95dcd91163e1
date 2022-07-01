678944720ba7637aa54ffc153232a90d5e5c1068
naveenpatnana <naveen_patnana@maplemonk.com>
2022-07-01 17:43:40 +0530

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "CREATE TABLE DEPARTMENT( ID INT PRIMARY KEY NOT NULL, DEPT CHAR(50) NOT NULL, EMP_ID INT NOT NULL );",
+                                "sql": "CREATE TABLE dev.maplemonk.DEPARTMENT( ID INT PRIMARY KEY NOT NULL, DEPT CHAR(50) NOT NULL, EMP_ID INT NOT NULL );",
                                 "transaction": true
                             }
                         ) }}
