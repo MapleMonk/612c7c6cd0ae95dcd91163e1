bb9b1d1abf258bde4c9d0fc7ed1c5a7abbf4855c
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-07-07 13:24:00 +0000

    model file has updated

--- models/example/UB_Loss.sql
+++ models/example/UB_Loss.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "ALTER TABLE eggozdb.maplemonk.ub_loss ADD (cdate Date); UPDATE eggozdb.maplemonk.ub_loss SET cdate = TRY_TO_DATE(\"Date \");",
+                                "sql": "ALTER TABLE eggozdb.maplemonk.ub_loss ADD (cdate Date); UPDATE eggozdb.maplemonk.ub_loss SET cdate = TRY_TO_DATE(\"Date \",\'dd/mm/yyyy\');",
                                 "transaction": true
                             }
                         ) }}
