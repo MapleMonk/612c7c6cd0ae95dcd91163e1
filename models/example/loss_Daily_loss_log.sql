49d93a03b2628fc9006108c354c4410111af91ff
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-07-12 09:40:45 +0000

    model file has updated

---
+++ models/example/loss_Daily_loss_log.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (ddate date); ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (eggs_sold numeric); ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (revenue numeric); UPDATE eggozdb.maplemonk.loss_daily_loss_log dl SET ddate = TRY_CONVERT(date,dl.date,103); UPDATE dl SET dl.eggs_sold = ds.eggs_sold FROM eggozdb.maplemonk.loss_daily_loss_log dl INNER JOIN eggozdb.maplemonk.summary_reporting_table_beat_retailer ds ON dl.ddate = ds.date; UPDATE dl SET dl.revenue = ds.net_sales FROM eggozdb.maplemonk.loss_daily_loss_log dl INNER JOIN eggozdb.maplemonk.summary_reporting_table_beat_retailer ds ON dl.ddate = ds.date;",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from EGGOZDB.MAPLEMONK.loss_Daily_loss_log
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
