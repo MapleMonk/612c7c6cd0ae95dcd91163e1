53d0413c4abda9329915e84db01ab27260b34f9a
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-07-12 07:09:07 +0000

    model file has updated

--- models/example/loss_Daily_loss_log.sql
+++ models/example/loss_Daily_loss_log.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (ddate date); ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (eggs_sold numeric); ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (revenue numeric); UPDATE eggozdb.maplemonk.loss_daily_loss_log dl SET ddate = CAST(dl.\"date\" as date); UPDATE dl SET dl.eggs_sold = ds.eggs_sold FROM eggozdb.maplemonk.loss_daily_loss_log dl INNER JOIN eggozdb.maplemonk.summary_reporting_table_beat_retailer ds ON dl.ddate = ds.date; UPDATE dl SET dl.revenue = ds.net_sales FROM eggozdb.maplemonk.loss_daily_loss_log dl INNER JOIN eggozdb.maplemonk.summary_reporting_table_beat_retailer ds ON dl.ddate = ds.date;",
+                                "sql": "ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (ddate date); ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (eggs_sold numeric); ALTER TABLE eggozdb.maplemonk.loss_daily_loss_log ADD (revenue numeric); UPDATE eggozdb.maplemonk.loss_daily_loss_log dl SET ddate = dl.date; UPDATE dl SET dl.eggs_sold = ds.eggs_sold FROM eggozdb.maplemonk.loss_daily_loss_log dl INNER JOIN eggozdb.maplemonk.summary_reporting_table_beat_retailer ds ON dl.ddate = ds.date; UPDATE dl SET dl.revenue = ds.net_sales FROM eggozdb.maplemonk.loss_daily_loss_log dl INNER JOIN eggozdb.maplemonk.summary_reporting_table_beat_retailer ds ON dl.ddate = ds.date;",
                                 "transaction": true
                             }
                         ) }}
