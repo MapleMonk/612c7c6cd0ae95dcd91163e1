a78fdfa217aa562e6729aefd30ed9c43a39681d4
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-07-27 10:17:23 +0000

    model file has updated

--- models/example/region_wise_procurement_MasterData.sql
+++ models/example/region_wise_procurement_MasterData.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (GRN_Date date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET GRN_Date = TRY_TO_DATE(\"GRN Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (Bill_Date date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET Bill_Date = TRY_TO_DATE(\"Bill Date\",\'DD/MM/YYYY\');",
+                                "sql": "ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (GRN_Date date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET GRN_Date = TRY_TO_DATE(\"GRN Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (Bill_Date date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET Bill_Date = TRY_TO_DATE(\"Bill Date\",\'YYYY-MM-DD\');",
                                 "transaction": true
                             }
                         ) }}
