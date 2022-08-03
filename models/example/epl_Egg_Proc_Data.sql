56e14ab2cb6b7ba09211434d1b9ed45255121a27
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-08-03 12:13:00 +0000

    model file has updated

---
+++ models/example/epl_Egg_Proc_Data.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "ALTER TABLE eggozdb.maplemonk.epl_egg_proc_data ADD (GRN_Date date); UPDATE eggozdb.maplemonk.epl_egg_proc_data SET GRN_Date = TRY_TO_DATE(\"GRN Date\",\'DD/MM/YYYY\');",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from EGGOZDB.MAPLEMONK.epl_Egg_Proc_Data
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
