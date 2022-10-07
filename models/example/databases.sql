76b465a379954786923add0e5c2ae9933b2594d1
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-10-07 13:12:54 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "CREATE OR REPLACE TABLE hilodesign_db.MAPLEMONK.GA_Order_By_Source_Consolidated_Intermediate AS select \'Shopify_India\' as Shop_Name, * from hilodesign_db.MAPLEMONK.ga_orders_by_source CREATE OR REPLACE TABLE hilodesign_db.MAPLEMONK.GA_ORDER_BY_SOURCE_CONSOLIDATED_HILO AS select GASC.* from hilodesign_db.MAPLEMONK.GA_Order_By_Source_Consolidated_Intermediate GASC;",
+                                "sql": "CREATE OR REPLACE TABLE hilodesign_db.MAPLEMONK.GA_Order_By_Source_Consolidated_Intermediate AS select \'Shopify_India\' as Shop_Name, * from hilodesign_db.MAPLEMONK.ga_orders_by_source; CREATE OR REPLACE TABLE hilodesign_db.MAPLEMONK.GA_ORDER_BY_SOURCE_CONSOLIDATED_HILO AS select GASC.* from hilodesign_db.MAPLEMONK.GA_Order_By_Source_Consolidated_Intermediate GASC;",
                                 "transaction": true
                             }
                         ) }}
