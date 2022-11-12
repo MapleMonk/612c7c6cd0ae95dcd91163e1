1bb46114023a667d5bb10f6384ecfb6b95136762
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-11-12 10:44:37 +0000

    model file has updated

--- models/example/EPM_Sheet1.sql
+++ models/example/EPM_Sheet1.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "ALTER TABLE eggozdb.maplemonk.epm_sheet1 ADD (LogDate Date); UPDATE eggozdb.maplemonk.epm_sheet1 SET LogDate = TRY_TO_DATE(\"DATE\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.epm_sheet1 ADD (Pdate Date); UPDATE eggozdb.maplemonk.epm_sheet1 SET Pdate = TRY_TO_DATE(\"Processing Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.epm_pkg_loss ADD (LogDate Date); UPDATE eggozdb.maplemonk.epm_pkg_loss SET LogDate = TRY_TO_DATE(\"DATE \",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.epm_pkg_loss ADD (Pdate Date); UPDATE eggozdb.maplemonk.epm_pkg_loss SET Pdate = TRY_TO_DATE(\"Packed Date\",\'DD/MM/YYYY\');",
+                                "sql": "ALTER TABLE eggozdb.maplemonk.epm_sheet1 ADD (LogDate Date); UPDATE eggozdb.maplemonk.epm_sheet1 SET LogDate = TRY_TO_DATE(\"DATE\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.epm_sheet1 ADD (Pdate Date); UPDATE eggozdb.maplemonk.epm_sheet1 SET Pdate = TRY_TO_DATE(\"Processing Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.epm_pkg_loss ADD (LogDate Date); UPDATE eggozdb.maplemonk.epm_pkg_loss SET LogDate = TRY_TO_DATE(\"Date \",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.epm_pkg_loss ADD (Pdate Date); UPDATE eggozdb.maplemonk.epm_pkg_loss SET Pdate = TRY_TO_DATE(\"Packed Date\",\'DD/MM/YYYY\');",
                                 "transaction": true
                             }
                         ) }}
