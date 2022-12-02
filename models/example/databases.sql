72408c53cd548973b3cdc3cbbd2350990800fd7e
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-12-02 17:54:27 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "Create or replace table gynoveda_db.maplemonk.asp_fact_items_gynoveda as SELECT *, CONVERT_TIMEZONE(\'UTC\',\'Asia/Kolkata\', \"purchase-date\":: DATETIME) as \"Purchase-datetime-IST\" FROM gynoveda_db.maplemonk.ASP_IN_GET_FLAT_FILE_ALL_ORDERS_DATA_BY_LAST_UPDATE_GENERAL WHERE upper(CURRENCY) = \'INR\' AND \"item-price\" NOT IN(\'\',\'0.0\');",
+                                "sql": "Create or replace table gynoveda_db.maplemonk.asp_fact_items_gynoveda as select ASP.* ,MAP.product Mapped_Product_Name ,MAP.category Mapped_Category ,MAP.product_type Product_type from (SELECT *, CONVERT_TIMEZONE(\'UTC\',\'Asia/Kolkata\', \"purchase-date\":: DATETIME) as \"Purchase-datetime-IST\" FROM gynoveda_db.maplemonk.ASP_IN_GET_FLAT_FILE_ALL_ORDERS_DATA_BY_LAST_UPDATE_GENERAL WHERE upper(CURRENCY) = \'INR\' AND \"item-price\" NOT IN(\'\',\'0.0\')) ASP left join gynoveda_db.maplemonk.ASIN_SKU_MASTER MAP on ASP.SKU = MAP.SKU and ASP.ASIN = MAP.ASIN ;",
                                 "transaction": true
                             }
                         ) }}
