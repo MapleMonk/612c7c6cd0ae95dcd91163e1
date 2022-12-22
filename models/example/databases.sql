51bdf218927b7fde83d0f34ce7f18ae98a327dd4
Naveen Patnana <naveen_patnana@maplemonk.com>
2022-12-22 10:31:13 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "create or replace table Google_ADS_Consolidated as select \'Shopify_USA\' as Region ,\"campaign.id\" ,\"campaign.name\" ,\"segments.date\" ,\"metrics.clicks\" ,\"campaign.status\" ,\"metrics.conversions\" ,\"metrics.cost_micros\" ,\"metrics.impressions\" ,\"metrics.conversions_value\" ,\"campaign.advertising_channel_type\" from VAHDAM_DB.MAPLEMONK.GOOGLE_ADS_US_CAMPAIGN_DATA union all select \'Shopify_Germany\' as Region ,\"campaign.id\" ,\"campaign.name\" ,\"segments.date\" ,\"metrics.clicks\" ,\"campaign.status\" ,\"metrics.conversions\" ,\"metrics.cost_micros\" ,\"metrics.impressions\" ,\"metrics.conversions_value\" ,\"campaign.advertising_channel_type\" from VAHDAM_DB.MAPLEMONK.GOOGLE_ADS_GERMANY_CAMPAIGN_DATA union all select \'Shopify_Global\' as Region ,\"campaign.id\" ,\"campaign.name\" ,\"segments.date\" ,\"metrics.clicks\" ,\"campaign.status\" ,\"metrics.conversions\" ,\"metrics.cost_micros\" ,\"metrics.impressions\" ,\"metrics.conversions_value\" ,\"campaign.advertising_channel_type\" from VAHDAM_DB.MAPLEMONK.GOOGLE_ADS_GLOBAL_CAMPAIGN_DATA union all select \'Shopify_India\' as Region ,\"campaign.id\" ,\"campaign.name\" ,\"segments.date\" ,\"metrics.clicks\" ,\"campaign.status\" ,\"metrics.conversions\" ,\"metrics.cost_micros\" ,\"metrics.impressions\" ,\"metrics.conversions_value\" ,\"campaign.advertising_channel_type\" from VAHDAM_DB.MAPLEMONK.gads_in_campaign_data;",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from VAHDAM_DB.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
