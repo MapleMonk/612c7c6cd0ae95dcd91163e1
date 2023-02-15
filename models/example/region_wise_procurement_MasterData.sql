94ddeeba6e11078a93cf09854cb15337f09a537e
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-02-15 10:31:33 +0000

    model file has updated

--- models/example/region_wise_procurement_MasterData.sql
+++ models/example/region_wise_procurement_MasterData.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (GRN_DATE Date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET GRN_DATE = TRY_TO_DATE(\"GRN Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (BILL_DATE Date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET BILL_DATE = TRY_TO_DATE(\"Bill Date\",\'DD/MM/YYYY\') UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET region = \'East\' where lower(region) in (\'kolkata\',\'bihar\') ;",
+                                "sql": "ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (GRN_DATE Date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET GRN_DATE = TRY_TO_DATE(\"GRN Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.region_wise_procurement_masterdata ADD (BILL_DATE Date); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET BILL_DATE = TRY_TO_DATE(\"Bill Date\",\'DD/MM/YYYY\'); UPDATE eggozdb.maplemonk.region_wise_procurement_masterdata SET region = \'East\' where lower(region) in (\'kolkata\',\'bihar\');",
                                 "transaction": true
                             }
                         ) }}
