bffa0ccce7f75ad0ec10b6a05da669f62b31b910
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-02-22 11:20:41 +0000

    model file has updated

---
+++ models/example/Replacement_Ageing_Noida.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "ALTER TABLE eggozdb.maplemonk.replacement_ageing_noida ADD (ddate Date); UPDATE eggozdb.maplemonk.replacement_ageing_noida SET ddate = TRY_TO_DATE(\"Receiving Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.replacement_ageing_delhi ADD (ddate Date); UPDATE eggozdb.maplemonk.replacement_ageing_delhi SET ddate = TRY_TO_DATE(\"Receiving Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.replacement_ageing_gurgaon ADD (ddate Date); UPDATE eggozdb.maplemonk.replacement_ageing_gurgaon SET ddate = TRY_TO_DATE(\"Receiving Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.replacement_ageing_bangalore ADD (ddate Date); UPDATE eggozdb.maplemonk.replacement_ageing_bangalore SET ddate = TRY_TO_DATE(\"Receiving Date\",\'DD/MM/YYYY\'); ALTER TABLE eggozdb.maplemonk.replacement_ageing_kolkata ADD (ddate Date); UPDATE eggozdb.maplemonk.replacement_ageing_kolkata SET ddate = TRY_TO_DATE(\"Receiving Date\",\'DD/MM/YYYY\');",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from EGGOZDB.MAPLEMONK.Replacement_Ageing_Noida
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
