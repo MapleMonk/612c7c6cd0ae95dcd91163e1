f8189b8efb6f10134a0807dacee8dcf6b5b621c4
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-06-29 14:23:02 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "delete from xyxx_db.maplemonk._airbyte_raw_fieldassist_detailed_visit where _AIRBYTE_DATA:\"VisitId\" > 800417234; delete from xyxx_db.maplemonk.fieldassist_detailed_visit_scd where visitid > 800417234; delete from xyxx_db.maplemonk.fieldassist_detailed_visit where visitid > 800417234; delete from xyxx_db.maplemonk.fieldassist_detailed_visit_sales where visitid > 800417234;",
+                                "sql": "delete from xyxx_db.maplemonk._airbyte_raw_fieldassist_detailed_visit where _AIRBYTE_DATA:\"VisitId\" > 958325899; delete from xyxx_db.maplemonk.fieldassist_detailed_visit_scd where visitid > 958325899; delete from xyxx_db.maplemonk.fieldassist_detailed_visit where visitid > 958325899; delete from xyxx_db.maplemonk.fieldassist_detailed_visit_sales where visitid > 958325899;",
                                 "transaction": true
                             }
                         ) }}
