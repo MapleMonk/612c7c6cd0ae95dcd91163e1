6fe611a1b854e7358300077052ee38e16757da91
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-07-17 20:18:23 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "create or replace table MYMUSE_DB.MAPLEMONK.Shopify_UTM_Parameters_fact_item as select * from MYMUSE_DB.MAPLEMONK.Shopify_muse_india_Shopify_UTM_Parameters ;",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from MYMUSE_DB.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
