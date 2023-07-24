41947adfea8e3417371973659fc7437c5759bbe0
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-07-24 14:21:13 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.order_point_inventory as select *, CASE WHEN OP < total_units_on_hand THEN 0 ELSE 1 END as inventory_below_order_point, CASE WHEN OP < total_units_on_hand THEN 0 ELSE ROUND(OP - total_units_on_hand) END as deficit_inventory FROM ( SELECT ORDER_POINT.*, total_units_on_hand FROM snitch_db.MAPLEMONK.ORDER_POINT LEFT JOIN ( SELECT SKU_GROUP, sum(INVENTORY_AVAILABLE) as total_units_on_hand from snitch_db.MAPLEMONK.INVENTORY_SUMMARY_SNITCH GROUP by sku_group order by total_units_on_hand desc ) as inventory_on_hand ON ORDER_POINT.sku_group = inventory_on_hand.sku_group WHERE OP is not null order by OP desc ) WHERE deficit_inventory > 50 order by OP desc, deficit_inventory desc",
+                                "sql": "create or replace table snitch_db.maplemonk.order_point_inventory as select *, CASE WHEN OP < total_units_on_hand THEN 0 ELSE 1 END as inventory_below_order_point, CASE WHEN OP < total_units_on_hand THEN 0 ELSE ROUND(OP - total_units_on_hand) END as deficit_inventory FROM ( SELECT ORDER_POINT.*, total_units_on_hand FROM snitch_db.MAPLEMONK.ORDER_POINT LEFT JOIN ( SELECT SKU_GROUP, sum(INVENTORY_AVAILABLE) as total_units_on_hand from snitch_db.MAPLEMONK.INVENTORY_SUMMARY_SNITCH GROUP by sku_group order by total_units_on_hand desc ) as inventory_on_hand ON ORDER_POINT.sku_group = inventory_on_hand.sku_group WHERE OP is not null order by OP desc ) order by OP desc, deficit_inventory desc",
                                 "transaction": true
                             }
                         ) }}
