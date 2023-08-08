5b9436b6a7076ac5f3482a32bbc40b0558e89cfb
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-08-08 12:08:09 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table indusvalley_db.MAPLEMONK.Shopify_UTM_Parameters_fact_item as select * from indusvalley_db.MAPLEMONK.Shopify_good_roots_kitchenware_private_limited_Shopify_UTM_Parameters ;",
+                                "sql": "create or replace table indusvalley_db.MAPLEMONK.Shopify_UTM_Parameters_fact_item as select * from indusvalley_db.MAPLEMONK.INDUSVALLEY_DB_SHOPIFY_UTM_PARAMETERS;",
                                 "transaction": true
                             }
                         ) }}
