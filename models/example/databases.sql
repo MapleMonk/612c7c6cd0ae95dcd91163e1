a77ba6fb1286c48b63afa7fe90f1daf6c8a040dc
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-08-17 08:34:01 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.store_wise_daily_orders as select SKU, SKU_GROUP, marketplace_mapped, coalesce(sum(suborder_quantity),0) as total_units_sold, coalesce(sum(selling_price),0) as total_sales, coalesce(sum(return_quantity),0) as total_return_quantity, coalesce(sum(discount),0) as total_discount, order_date, extract (month from order_date) as month FROM snitch_db.MAPLEMONK.UNICOMMERCE_FACT_ITEMS_SNITCH WHERE source in (\'POS1\') GROUP BY sku, sku_group, marketplace_mapped, order_date, month ORDER BY total_sales desc",
+                                "sql": "create or replace table snitch_db.maplemonk.store_wise_daily_orders as select SKU, SKU_GROUP, category, coalesce(sum(suborder_quantity),0) as total_units_sold, coalesce(sum(selling_price),0) as total_sales, coalesce(sum(return_quantity),0) as total_return_quantity, coalesce(sum(discount),0) as total_discount, order_date, extract (month from order_date) as month, SUBSTRING(sku, LEN(sku) - CHARINDEX(\'-\', REVERSE(sku)) + 2, LEN(sku)) AS size FROM snitch_db.MAPLEMONK.UNICOMMERCE_FACT_ITEMS_SNITCH WHERE source in (\'POS1\') GROUP BY sku, sku_group, order_date, month,category, size",
                                 "transaction": true
                             }
                         ) }}
