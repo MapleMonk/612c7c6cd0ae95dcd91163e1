ae5f5e29a71352450756270ce33bc2e89c7a71ba
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-08-17 08:11:16 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "create or replace table snitch_db.maplemonk.store_wise_daily_orders as select SKU, SKU_GROUP, marketplace_mapped, coalesce(sum(suborder_quantity),0) as total_units_sold, coalesce(sum(selling_price),0) as total_sales, coalesce(sum(return_quantity),0) as total_return_quantity, coalesce(sum(discount),0) as total_discount, order_date, extract (month from order_date) as month FROM snitch_db.MAPLEMONK.UNICOMMERCE_FACT_ITEMS_SNITCH WHERE source in (\'POS1\') GROUP BY sku, sku_group, marketplace_mapped, order_date, month ORDER BY total_sales desc",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from snitch_db.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
