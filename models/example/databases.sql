6a452d401272b95f80761deeefd1bd7661e483d2
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-08-21 10:18:40 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "create or replace table snitch_db.maplemonk.future_inwarding as WITH LatestProductDetails AS ( SELECT SKU_GROUP, PRODUCT_NAME, CATEGORY, ROW_NUMBER() OVER(PARTITION BY SKU_GROUP ORDER BY order_date DESC) as row_num FROM snitch_db.MAPLEMONK.UNICOMMERCE_FACT_ITEMS_SNITCH ) , ExpectedDelivery AS ( SELECT SKU_group, CAST(expected_delivery_date AS date) as date_of_delivery, SUM(quantity) as expected_qty FROM snitch_db.MAPLEMONK.FACTORY_PRODUCTION_INVENTORY GROUP BY sku_group, expected_delivery_date ) SELECT ed.SKU_group, ed.date_of_delivery, ed.expected_qty, lpd.PRODUCT_NAME, lpd.CATEGORY FROM ExpectedDelivery ed LEFT JOIN LatestProductDetails lpd ON ed.SKU_group = lpd.SKU_GROUP WHERE lpd.row_num = 1 ORDER BY ed.date_of_delivery",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from snitch_db.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
