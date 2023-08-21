cac19b51aa1cb125f1aa7b31c85aeab2abf0a88d
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-08-21 12:45:56 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.180_days_inventory_deep_dive as WITH InventoryDetails AS ( SELECT sku_group, product_name, days_in_warehouse, SUM(units_on_hand) as total_units_on_hand FROM snitch_db.MAPLEMONK.INVENTORY_AGING_BUCKETS_SNITCH GROUP BY sku_group, product_name, days_in_warehouse ) SELECT id.sku_group, id.product_name, id.days_in_warehouse, id.total_units_on_hand, eoq.* FROM InventoryDetails id LEFT JOIN snitch_db.MAPLEMONK.EOQ eoq ON id.sku_group = eoq.sku_group ORDER BY id.total_units_on_hand DESC",
+                                "sql": "create or replace table snitch_db.maplemonk.old_180_inventory_deep_dive as WITH InventoryDetails AS ( SELECT sku_group, product_name, days_in_warehouse, SUM(units_on_hand) as total_units_on_hand FROM snitch_db.MAPLEMONK.INVENTORY_AGING_BUCKETS_SNITCH GROUP BY sku_group, product_name, days_in_warehouse ) SELECT id.sku_group, id.product_name, id.days_in_warehouse, id.total_units_on_hand, eoq.* FROM InventoryDetails id LEFT JOIN snitch_db.MAPLEMONK.EOQ eoq ON id.sku_group = eoq.sku_group ORDER BY id.total_units_on_hand DESC",
                                 "transaction": true
                             }
                         ) }}
