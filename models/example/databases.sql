3bcc1798bdaa6325392ca9ef832c51a1d0bf7ba5
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-08-21 12:42:29 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "WITH InventoryDetails AS ( SELECT sku_group, product_name, days_in_warehouse, SUM(units_on_hand) as total_units_on_hand FROM snitch_db.MAPLEMONK.INVENTORY_AGING_BUCKETS_SNITCH GROUP BY sku_group, product_name, days_in_warehouse ) SELECT id.sku_group, id.product_name, id.days_in_warehouse, id.total_units_on_hand, eoq.* FROM InventoryDetails id LEFT JOIN snitch_db.MAPLEMONK.EOQ eoq ON id.sku_group = eoq.sku_group ORDER BY id.total_units_on_hand DESC",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from snitch_db.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
