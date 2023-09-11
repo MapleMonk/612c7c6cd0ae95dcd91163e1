342613875780874db9650eebd6d5f8259a85924c
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-09-11 16:03:40 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace snitch_db.maplemonk.store_stock_aging as select branch_name,logicusercode, TO_DATE(replace(lot_pur_date,\'\"\',\'\'),\'DD/MM/YYYY\')::date as date_inward, sum(stock_qty) as total_qty from snitch_db.maplemonk.logicerp_get_stock_in_hand group by 1,2,3 order by 1,3",
+                                "sql": "create or replace snitch_db.maplemonk.store_stock_aging as select branch_name,logicusercode, TO_DATE(replace(lot_pur_date,\'\"\',\'\'),\'DD/MM/YYYY\') as date_inward, sum(stock_qty) as total_qty from snitch_db.maplemonk.logicerp_get_stock_in_hand group by branch_name, logicusercode,date_inward order by date_inward",
                                 "transaction": true
                             }
                         ) }}
