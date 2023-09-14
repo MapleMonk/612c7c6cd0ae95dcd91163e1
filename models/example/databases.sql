36ed6b9a07d98e158ba5843d85cb5aa618f8fb75
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-09-14 13:18:48 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table dunatura_db.maplemonk.Shopify_UTM_Parameters_fact_item as select * from dunatura_db.maplemonk.Shopify_dunatura_de_UTM_Parameters ;",
+                                "sql": "create or replace table dunatura_db.maplemonk.Shopify_UTM_Parameters_fact_item as select * from dunatura_db.maplemonk.Shopify_dunatura_de_Shopify_UTM_Parameters ;",
                                 "transaction": true
                             }
                         ) }}
