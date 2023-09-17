b9d286788fbdeb07f6630b06ca4e316f30dafb2d
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-09-17 12:25:05 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.suspicious_orders as select date(order_timestamp) as dt, order_name, customer_id,customer_name, payment_gateway, sum(line_item_sales) as total_value from snitch_db.maplemonk.fact_items_snitch where dt >= date(getdate())-2 group by dt, order_name,customer_id,customer_name, payment_gateway order by total_value desc",
+                                "sql": "create or replace table snitch_db.maplemonk.suspicious_orders as select date(order_timestamp) as dt, order_name, customer_id, customer_name, payment_gateway, payment_method, checkout, bnpl_flag, source, sum(line_item_sales) as line_item_total, sum(net_sales) as net_total, sum(gross_sales) as gross_total from snitch_db.maplemonk.fact_items_snitch where dt >= date(getdate())-2 group by 1,2,3,4,5,6,7,8,9 order by line_item_total desc",
                                 "transaction": true
                             }
                         ) }}
