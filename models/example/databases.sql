73e61acdd62168137558e06acd6e2609283051c8
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-10-03 06:22:40 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table prd_db.beardo.dwh_ecommerce_summary as select to_date(date, \'mm/dd/yyyy\') ordeR_date, channel_group, Channel_name sku_code, quantity::float quantity, total_mrp::float total_mrp, \"BD Business\" product_category, \"BD Category\" Product_sub_category, product_name, \"NR (Single)\" NR_per_unit, \" NR (Total) \" NR from datalake_db.beardo.trn_ecomm_offtake",
+                                "sql": "create or replace table prd_db.beardo.dwh_ecommerce_summary as select to_date(date, \'mm/dd/yyyy\') ordeR_date, channel_group, Channel_name, sku_code, quantity::float quantity, total_mrp::float total_mrp, \"BD Business\" product_category, \"BD Category\" Product_sub_category, product_name, (case when replace(\"NR (Single)\",\',\',\'\') = \' - \' then 0 else replace(\"NR (Single)\",\',\',\'\') end)::float NR_per_unit, (case when replace(\" NR (Total) \",\',\',\'\') = \' - \' then 0 else replace(\" NR (Total) \",\',\',\'\') end)::float NR from datalake_db.beardo.trn_ecomm_offtake",
                                 "transaction": true
                             }
                         ) }}
