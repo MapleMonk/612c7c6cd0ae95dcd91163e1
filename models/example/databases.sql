05648ca7c30ba5569ab3192a8478be5dbf2ab0f3
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-10-09 07:40:30 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.unicommerce_availability_merge as select fa.*,am.sku_class from snitch_db.maplemonk.unicommerce_fact_items_snitch fa left join snitch_db.maplemonk.availability_master am on fa.sku_group = am.sku_group",
+                                "sql": "create or replace table snitch_db.maplemonk.unicommerce_availability_merge as SELECT fa.*, am.sku_class, sgt.sleeve_type, sgt.collar_type, sgt.fabric, sgt.design, sgt.hem, sgt.closure, sgt.fit, sgt.occassion, sgt.product_type FROM snitch_db.maplemonk.unicommerce_fact_items_snitch fa LEFT JOIN snitch_db.maplemonk.availability_master am ON fa.sku_group = am.sku_group LEFT JOIN snitch_db.maplemonk.sku_group_tags sgt ON fa.sku_group = sgt.sku_group",
                                 "transaction": true
                             }
                         ) }}
