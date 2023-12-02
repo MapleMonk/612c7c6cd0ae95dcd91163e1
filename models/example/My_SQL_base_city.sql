005cd7cbd132cf8ed50f11a3189c11d98fb4f86d
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-12-02 16:40:09 +0000

    model file has updated

---
+++ models/example/My_SQL_base_city.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "create or replace table eggozdb.maplemonk.my_sql_product_product as select pp.*, case when pp.brand_type = \'branded\' then (case when pp.short_name in (\'WE\',\'WEE\',\'WD\',\'FR\',\'CH\') then pp.description when pp.short_name = \'B\' then \'Eggoz Brown\' when pp.short_name = \'W\' then \'Eggoz White\' when pp.short_name = \'N\' then \'Eggoz Nutra\' when pp.productsubdivision_id in (42,45) then pps.name else \'Branded Others\' end) else \'Unbranded\' end as product_type from eggozdb.maplemonk.my_sql_product_product pp left join eggozdb.maplemonk.my_sql_product_productsubdivision pps on pps.id = pp.productSubDivision_id ;",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from EGGOZDB.MAPLEMONK.My_SQL_base_city
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
