48549494202e4b29a108d5468bdeca8cd9866ac5
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-12-12 10:59:47 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.stuck_orders as select * from snitch_db.maplemonk.unicommerce_fact_items_snitch where order_status in (\'PROCESSING\') and SHIPPING_STATUS in (\'PACKED\') and order_date <date(getdate())-1 and order_date >date(getdate())-90",
+                                "sql": "create or replace table snitch_db.maplemonk.stuck_orders as select ufc.*, fc.payment_method from snitch_db.maplemonk.unicommerce_fact_items_snitch ufc left join snitch_db.maplemonk.fact_items_snitch fc on ufc.order_id = fc.order_id and fc.line_item_id=split_part(ufc.saleorderitemcode,\'-\',0) where ufc.order_status in (\'PROCESSING\') and SHIPPING_STATUS in (\'PACKED\') and order_date <date(getdate())-1 and order_date >date(getdate())-90",
                                 "transaction": true
                             }
                         ) }}
