5e10a2856eafac03ec740a20ec44302b99d86305
Naveen Patnana <naveen_patnana@maplemonk.com>
2023-12-22 07:39:04 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.myntra_returns_processed as SELECT Style_id,seller_sku_code, MAX(return_reason) AS Highest_Reason FROM snitch_db.maplemonk.Myntra_Myntra_Returns GROUP BY Style_id,seller_sku_code",
+                                "sql": "create or replace table snitch_db.maplemonk.myntra_returns_processed as SELECT Style_id, seller_sku_code, MAX(return_reason) AS Highest_Reason, REVERSE(SUBSTRING(REVERSE(seller_sku_code), CHARINDEX(\'-\', REVERSE(seller_sku_code)) + 1)) AS sku_group FROM snitch_db.maplemonk.Myntra_Myntra_Returns GROUP BY Style_id,seller_sku_code,sku_group",
                                 "transaction": true
                             }
                         ) }}
