097b4135d41acc9eb652b74c0f239d862b3587d2
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-01-03 14:34:19 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "create or replace table hox_db.maplemonk.HOX_BLANKO_Easyecom_Tax_Fact_Items as select \'Easyecom sale\' as report_type, \"Company Name\" as Company_Name, \"Seller GST Num\" as seller_gst, \"MP Name\" as marketplace, \"Reference Code\" as Reference_Code, \"Order Type\" as order_type, \"Order Status\" as Order_status, \"Invoice Status\" as Invoice_status, try_cast(\"Invoice Date\" as timestamp) as Report_date, try_cast(\"Item Quantity\" as float) as Quantity, replace(\"Parent SKU\", \'`\', \'\') as SKU, \"Component SKU Name\" as component_sku_name, try_cast(\"Order Invoice Amount\" as float) as Invoice_amount, try_cast(\"Selling Price\" as float) as Selling_price, abs(try_cast(\"Order Invoice Amount\" as float)) as absolute_invoice_amount, try_cast(\"Taxable Value\" as float) as Tax_exclusive_gross, try_cast(\"TAX\" as float) as Total_tax_amount from HOX_DB.MAPLEMONK.easyecom_hox_blanko_tax_sales union all select \'Easyecom return\' as report_type, \"Company Name\" as Company_Name, \"Seller GST Num\" as seller_gst, \"MP Name\" as marketplace, \"Reference Code\" as Reference_Code, \"Order Type\" as order_type, \"Order Status\" as Order_status, \"Invoice Status\" as Invoice_status, try_cast(\"Return Date\" as timestamp) as Report_date, try_cast(\"Item Quantity\" as float) as Quantity, replace(\"Parent SKU\", \'`\', \'\') as SKU, \"Component SKU Name\" as component_sku_name, (try_cast(\"Order Invoice Amount\" as float) * -1.00) as Invoice_amount, try_cast(\"Selling Price\" as float) as Selling_price, abs(try_cast(\"Order Invoice Amount\" as float)) as absolute_invoice_amount, try_cast(\"Taxable Value\" as float) as Tax_exclusive_gross, try_cast(\"TAX\" as float) as Total_tax_amount from HOX_DB.MAPLEMONK.easyecom_hox_blanko_tax_returns;",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from HOX_DB.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
