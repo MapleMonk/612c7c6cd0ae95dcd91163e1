225dba324c9c2cf6fc8c2dcd3887be411656aea4
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-02-03 06:39:12 +0000

    model file has updated

--- models/example/transport_costs_last mid mile.sql
+++ models/example/transport_costs_last mid mile.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "ALTER TABLE eggozdb.maplemonk.transport_costs_last_mid_mile ADD (ddate Date); UPDATE eggozdb.maplemonk.transport_costs_last_mid_mile SET ddate = TRY_TO_DATE(Date,\'DD/MM/YYYY\');",
+                                "sql": "ALTER TABLE eggozdb.maplemonk.transport_costs_fm_vehicle_details_after_1st_april ADD (ddate Date); UPDATE eggozdb.maplemonk.transport_costs_fm_vehicle_details_after_1st_april SET ddate = TRY_TO_DATE(Date,\'DD/MM/YYYY\');",
                                 "transaction": true
                             }
                         ) }}
