ee5ad96220caaac7d28756bd73d6270947c1e426
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-02-07 18:29:13 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table connectyai_db.connectyai.Pharma_Sample_Compliance_Fact_Items as select upper(COUNTRY) COUNTRY ,Upper(CLASS) CLASS ,upper(molecule) MOLECULE ,upper(\"Type of Drug\") Type_OF_DRUG ,YEAR ,try_cast(replace(Compliance,\'%\',\'\') as float)/100 Compliance from connectyai_db.connectyai.PHARMA_SAMPLE_COMPLIANCE UNPIVOT(Compliance FOR Year IN (\"2017\",\"2018\",\"2019\",\"2020\",\"2021\",\"2022\",\"2023\",\"2024\",\"2025\",\"2026\",\"2027\")) order by year, country, CLASS, MOLECULE, \"Type of Drug\";",
+                                "sql": "create or replace table connectyai_db.connectyai.Pharma_Sample_Compliance_Fact_Items as Select * from ( select upper(COUNTRY) COUNTRY ,Upper(CLASS) CLASS ,upper(molecule) MOLECULE ,upper(\"Type of Drug\") Type_OF_DRUG ,YEAR ,try_cast(replace(Compliance,\'%\',\'\') as float)/100 Compliance ,row_number() over (partition by COUNTRY, CLASS, MOLECULE, \"Type of Drug\", YEAR order by 1) rw from connectyai_db.connectyai.PHARMA_SAMPLE_COMPLIANCE UNPIVOT(Compliance FOR Year IN (\"2017\",\"2018\",\"2019\",\"2020\",\"2021\",\"2022\",\"2023\",\"2024\",\"2025\",\"2026\",\"2027\")) order by year, country, CLASS, MOLECULE, \"Type of Drug\" ) where rw = 1;",
                                 "transaction": true
                             }
                         ) }}
