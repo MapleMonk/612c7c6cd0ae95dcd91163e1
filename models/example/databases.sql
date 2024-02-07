6cb68ef1171145ec03b6014bb9b1b0898dd08277
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-02-07 18:27:03 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table connectyai_db.connectyai.Pharma_Sample_Sales_Fact_Items as select Upper(COUNTRY) COUNTRY ,Upper(CLASS) CLASS ,upper(molecule) MOLECULE ,upper(\"Type of Drug\") Type_OF_DRUG ,YEAR ,try_cast(SALES as float) SALES_IN_USD from connectyai_db.connectyai.pharma_sample_sales_in_mm_usd UNPIVOT(sales FOR Year IN (\"2017\",\"2018\",\"2019\",\"2020\",\"2021\",\"2022\",\"2023\",\"2024\",\"2025\",\"2026\",\"2027\")) order by year, country, CLASS, MOLECULE, \"Type of Drug\";",
+                                "sql": "create or replace table connectyai_db.connectyai.Pharma_Sample_Sales_Fact_Items as Select * from ( select Upper(COUNTRY) COUNTRY ,Upper(CLASS) CLASS ,upper(molecule) MOLECULE ,upper(\"Type of Drug\") Type_OF_DRUG ,YEAR ,try_cast(SALES as float) SALES_IN_USD ,row_number() over (partition by COUNTRY, CLASS, MOLECULE, \"Type of Drug\", YEAR order by 1) rw from connectyai_db.connectyai.pharma_sample_sales_in_mm_usd UNPIVOT(sales FOR Year IN (\"2017\",\"2018\",\"2019\",\"2020\",\"2021\",\"2022\",\"2023\",\"2024\",\"2025\",\"2026\",\"2027\")) order by year, country, CLASS, MOLECULE, \"Type of Drug\" ) where rw = 1;",
                                 "transaction": true
                             }
                         ) }}
