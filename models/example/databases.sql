b90371b9e8c0f4bad1aa251c16253a032b318662
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-02-07 18:17:00 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "create or replace table connectyai_db.connectyai.Pharma_Sample_Treated_Days_Fact_Items as select upper(COUNTRY) COUNTRY ,Upper(CLASS) CLASS ,upper(molecule) MOLECULE ,upper(\"Type of Drug\") Type_OF_DRUG ,YEAR ,try_cast(replace(Treated_Days,\',\',\'\') as integer) Treated_Days from connectyai_db.connectyai.pharma_sample_Treated_Days UNPIVOT(Treated_Days FOR Year IN (\"2017\",\"2018\",\"2019\",\"2020\",\"2021\",\"2022\",\"2023\",\"2024\",\"2025\",\"2026\",\"2027\")) order by year, country, CLASS, MOLECULE, \"Type of Drug\";",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from CONNECTYAI_DB.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
