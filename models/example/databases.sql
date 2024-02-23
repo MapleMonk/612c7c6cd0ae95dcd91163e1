ef35fb0025b31362c52fcda9303c924aa98f0c2d
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-02-23 09:53:14 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "Create or replace table ABC_ACQUISITION_SOURCE_ALL_DATA AS SELECT src.*, Call.\"Number Count\", Call.\"Avg Talk Time (mins)\", Call.\"Total Call Duration (hh mm ss)\", Wsap.\"TOTAL_MSG_SENT\", Wsap.\"PHNNUMBER\", Wsap. \"NUMBER\" FROM SELECT_DB.MAPLEMONK.CLEANED_ABC_ABANDONED_CHECKOUTS AS src JOIN SELECT_DB.MAPLEMONK.ABC_ABNDED_CLEAN_DF1__3__CSV AS Call ON CAST(src.PHONE AS INT) = CAST(Call.CUSTOMER AS INT) JOIN ABC_W_OUTPUT_FILE_CSV AS Wsap ON CAST(src.PHONE AS INT) = CAST(Wsap.PHNNUMBER AS INT);",
+                                "sql": "Create or replace table ABC_ACQUISITION_SOURCE_ALL_DATA AS SELECT src.*, Call.\"Number Count\", Call.\"Avg Talk Time (mins)\", Call.\"Total Call Duration (hh mm ss)\", Wsap.\"TOTAL_MSG_SENT\", Wsap.\"PHNNUMBER\", Wsap. \"NUMBER\" FROM SELECT_DB.MAPLEMONK.ABC_ACQUISITION_SOURCE AS src JOIN SELECT_DB.MAPLEMONK.ABC_ABNDED_CLEAN_DF1__3__CSV AS Call ON CAST(src.PHONE AS INT) = CAST(Call.CUSTOMER AS INT) JOIN ABC_W_OUTPUT_FILE_CSV AS Wsap ON CAST(src.PHONE AS INT) = CAST(Wsap.PHNNUMBER AS INT);",
                                 "transaction": true
                             }
                         ) }}
