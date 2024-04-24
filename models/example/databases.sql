6a082556ad91f00e9b10dce6d6ed533351ac3929
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-04-24 05:12:58 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "Create or replace table italiancolony_db.maplemonk.forward_shipment_reconciliation as select order_date::Date order_Date, reference_code ordeR_name, a.awb, sku, dispatch_date::Date dispatch_date, b.current_status clickpost_status, getdate()::Date - dispatch_date::Date days_since_dispatch from italiancolony_db.MAPLEMONK.italiancolony_db_unicommerce_fact_items a left join (select awb_number ,current_status from italiancolony_db.MAPLEMONK.italiancolony_DB_CLICKPOST_FACT_ITEMS )b on a.awb = b.awb_number where current_status not in (\'RTO-Delivered\',\'Delivered\') and getdate()::Date - dispatch_date::Date >=15 ;",
+                                "sql": "Create or replace table italiancolony_db.maplemonk.forward_shipment_reconciliation as select order_date::Date order_Date, c.shopify_order_date, reference_code ordeR_name, a.awb, sku, dispatch_date::Date dispatch_date, b.current_status clickpost_status, getdate()::Date - dispatch_date::Date days_since_dispatch from italiancolony_db.MAPLEMONK.italiancolony_db_unicommerce_fact_items a left join (select awb_number ,current_status from italiancolony_db.MAPLEMONK.italiancolony_DB_CLICKPOST_FACT_ITEMS )b on a.awb = b.awb_number left join (select distinct ordeR_name, order_timestamp::date shopify_order_date from italiancolony_db.maplemonk.italiancolony_db_SHOPIFY_FACT_ITEMS)c on a.reference_code = c.order_name where current_status not in (\'RTO-Delivered\',\'Delivered\') and getdate()::Date - dispatch_date::Date >=15 ;",
                                 "transaction": true
                             }
                         ) }}
