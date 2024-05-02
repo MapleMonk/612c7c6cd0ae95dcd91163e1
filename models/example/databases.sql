38951eca1a166e089860718b73da7f01d27e7bf2
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-05-02 09:53:38 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.unicommerce_inventory_aging_day_on_day as select * from snitch_db.maplemonk.unicommerce_inventory_aging_day_on_day union select * from snitch_db.maplemonk.unicommerce_inventory_aging",
+                                "sql": "create or replace table snitch_db.maplemonk.unicommerce_inventory_aging_day_on_day as select * from snitch_db.maplemonk.unicommerce_inventory_aging_day_on_day union select * from snitch_db.maplemonk.unicommerce_inventory_aging; create or replace table snitch_db.maplemonk.unicommerce_inventory_aging_day_on_day as select * from ( select *,dense_rank() over(partition by \"Item Type skuCode\",_airbyte_emitted_at::date order by _airbyte_emitted_at asc)rw from snitch_db.maplemonk.unicommerce_inventory_aging_day_on_day ) where rw=1;",
                                 "transaction": true
                             }
                         ) }}
