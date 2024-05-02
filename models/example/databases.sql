2d164487f40d4e16af03ec2c0bfacc39f95bc6f9
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-05-02 04:31:58 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "CREATE OR REPLACE TABLE BSC_DB.Maplemonk.EngravingTags AS select name, created_at, tags, A.VALUE:sku::STRING sku, B.Value:value::string as EngraveData from BSC_DB.MAPLEMONK.Shopify_All_orders, LATERAL FLATTEN (INPUT => LINE_ITEMS)A where A.VALUE:sku::STRING in (\'PSS100\',\'SHAVE_6PSS_SENSI_LUXE_KIT\',\'SHAVE_SENSI_LUXE_RAZOR\',\'RB100\', \'SHAVE_RB100_BLACK\', \'SHAVE_PSS100_BLACK\') and A.value:properties != \'\'",
+                                "sql": "CREATE OR REPLACE TABLE BSC_DB.Maplemonk.EngravingTags AS select name, created_at, tags, A.VALUE:sku::STRING sku, B.Value:value::string as EngraveData from BSC_DB.MAPLEMONK.Shopify_All_orders, LATERAL FLATTEN (INPUT => LINE_ITEMS)A, LATERAL FLATTEN (INPUT => A.value:properties)B where A.VALUE:sku::STRING in (\'PSS100\',\'SHAVE_6PSS_SENSI_LUXE_KIT\',\'SHAVE_SENSI_LUXE_RAZOR\',\'RB100\', \'SHAVE_RB100_BLACK\', \'SHAVE_PSS100_BLACK\') and A.value:properties != \'\'",
                                 "transaction": true
                             }
                         ) }}
