7f84c54d08a77f7b34fcfde3b55ed187b37c68f6
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-05-22 10:05:32 +0000

    model file has updated

--- models/example/MY_SQL_retailer_retailer.sql
+++ models/example/MY_SQL_retailer_retailer.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table eggozdb.maplemonk.my_sql_tertiary_retailer_retailer as select rr.* from eggozdb.maplemonk.my_sql_tertiary_retailer_retailer rr left join eggozdb.maplemonk.my_sql_retailer_customer_category rcc on rcc.id = rr.category_id left join eggozdb.maplemonk.my_sql_retailer_customer_subcategory rcs on rcs.id = rr.sub_category_id where rr.dealer_id is not null ;",
+                                "sql": "create or replace table eggozdb.maplemonk.my_sql_tertiary_retailer_retailer as select rr.*,r1.code as dealer, r2.code as distributor from eggozdb.maplemonk.my_sql_tertiary_retailer_retailer rr left join eggozdb.maplemonk.my_sql_retailer_retailer r1 on rr.dealer_id = r1.id left join eggozdb.maplemonk.my_sql_retailer_retailer r2 on r1.distributor_id = r2.id; left join eggozdb.maplemonk.my_sql_retailer_customer_category rcc on rcc.id = rr.category_id left join eggozdb.maplemonk.my_sql_retailer_customer_subcategory rcs on rcs.id = rr.sub_category_id where rr.dealer_id is not null ;",
                                 "transaction": true
                             }
                         ) }}
