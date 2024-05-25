334344ec9d7d9bd156263350a69ffc0f5b95358f
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-05-25 10:19:41 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "CREATE or replace table select_db.maplemonk.orm_data as select ASIN ,to_date(DATE,\'DD/MM/YYYY\' )::date as date ,SC_SALES ,SC_UNITS ,VC_SALES ,VC_UNITS ,DF_RATING ,DF_REVIEW ,CALL_COUNT ,\"(Child) ASIN\" ,\"Review Count\" ,\"Reviews: Rating\" ,\"Reviews: Review Count\" from select_db.maplemonk.select_db_orm_data",
+                                "sql": "CREATE or replace table select_db.maplemonk.orm_data as select ASIN ,to_date(DATE,\'DD/MM/YYYY\' )::date as date ,REPLACE(SC_SALES, \',\', \'\')::int AS SC_SALES, ,SC_UNITS ,REPLACE(VC_SALES, \',\', \'\')::int AS VC_SALES, ,VC_UNITS ,DF_RATING ,DF_REVIEW ,CALL_COUNT ,\"(Child) ASIN\" ,\"Review Count\" ,\"Reviews: Rating\" ,\"Reviews: Review Count\" from select_db.maplemonk.select_db_orm_data",
                                 "transaction": true
                             }
                         ) }}
