b0a70113b32a36a453d5e5ce9c6d8d4c33cb705c
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-06-01 13:39:52 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.ItemBarcode_Daily_Report AS WITH DateCheck AS ( SELECT 1 AS Exist FROM snitch_db.maplemonk.ItemBarcode_Daily_Report WHERE DATE = CURRENT_DATE() LIMIT 1 ) SELECT CONVERT_TIMEZONE(\'UTC\', \'Asia/Kolkata\', _airbyte_normalized_at::DATETIME)::date AS date, \"Item Status\", \"Inventory type\", \"Facility Id\", SUM(\"Unit price without tax\") AS VALUE, COUNT(DISTINCT \"Item Code\") AS Units FROM snitch_db.maplemonk.unicommerce_itembarcode_report WHEre not EXISTS (SELECT * FROM DateCheck) GROUP BY 1, 2, 3, 4 union all select * FROM snitch_db.maplemonk.ItemBarcode_Daily_Report;",
+                                "sql": "ALTER SESSION SET TIMEZONE = \'Asia/Kolkata\'; create or replace table snitch_db.maplemonk.ItemBarcode_Daily_Report AS WITH DateCheck AS ( SELECT 1 AS Exist FROM snitch_db.maplemonk.ItemBarcode_Daily_Report WHERE DATE = CURRENT_DATE() LIMIT 1 ) SELECT CONVERT_TIMEZONE(\'UTC\', \'Asia/Kolkata\', _airbyte_normalized_at::DATETIME)::date AS date, \"Item Status\", \"Inventory type\", \"Facility Id\", SUM(\"Unit price without tax\") AS VALUE, COUNT(DISTINCT \"Item Code\") AS Units FROM snitch_db.maplemonk.unicommerce_itembarcode_report WHEre not EXISTS (SELECT * FROM DateCheck) GROUP BY 1, 2, 3, 4 union all select * FROM snitch_db.maplemonk.ItemBarcode_Daily_Report;",
                                 "transaction": true
                             }
                         ) }}
