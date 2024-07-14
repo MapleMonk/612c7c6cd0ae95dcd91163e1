546b5fd312bba661dc0a2a8e014831f552b78b66
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-07-14 22:07:10 +0530

    push7

--- models/example/deduping_generic_script.sql
+++ models/example/deduping_generic_script.sql
@@ -1,73 +1,9 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
 
-    Try changing "table" to "view" below
-*/
-
--- {{ config(materialized='table', alias= var('table') + "_test") }}
-
--- {{ config(materialized='table', alias = "tester_test") }}
-
--- with 
--- input_data as (
-
--- {% if var('customBquery', None) is not none %}
---     {{ var('customBquery') }}
--- {% else %}
---     select {{ var('rows') }} from {{var('table')}}
--- {% endif %}
-
--- ),
---  new_updated as (
---    SELECT * FROM (
---         select *, row_number() over(
---             partition by {{var("partitionRows")}}
---             order by 
---             {{var("cursor_feild")}} is null asc,
---             {{var("cursor_feild")}} desc,
---             _AIRBYTE_EMITTED_AT desc
---         ) AS ROW_NUMBER
---       FROM input_data
---      ) WHERE ROW_NUMBER = 1
--- )
--- SELECT {{var("orignalField")}}  FROM new_updated
-
-
-
--- with 
--- input_data as (
-
--- select * from maplemonk.TODELETE_CUSTOMERS
-
--- )
---  new_updated as (
---    SELECT * FROM (
---         select *, row_number() over(
---             partition by id
---             order by 
---             UPDATED_AT is null asc,
---             UPDATED_AT desc,
---             _AIRBYTE_EMITTED_AT desc
---         ) AS ROW_NUMBER
---       FROM input_data
---      ) WHERE ROW_NUMBER = 1
--- )
--- SELECT NOTE,ADDRESSES,LAST_ORDER_NAME  FROM input_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
-
-{{ config(materialized='table',schema = "maplemonk") }}
+{{ config(materialized='table') }}
 
 with input_data as (
-    select * from {{ ref('toDelete_customers') }}
-    -- {{ source('maplemonk', 'toDelete_customers') }}
+    select * from maplemonk.toDelete_customers
 ),
 new_updated as (
     select 
