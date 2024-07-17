5c4d456e58d5258ec5e1fb8d433531036fa18627
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-07-17 07:28:14 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
                         materialized='table',
                             post_hook={
-                                "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_high_discount AS ( WITH high_discount AS ( SELECT order_name, order_timestamp::date AS date, div0(SUM(discount), SUM(discount + gross_sales)) * 100 AS discount_percentage FROM snitch_db.maplemonk.fact_items_snitch GROUP BY order_name, date ) SELECT date, COUNT(order_name) AS order_count FROM high_discount GROUP BY date ); CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_superlow_value AS ( WITH low_value AS ( SELECT order_name, order_timestamp::date AS date, SUM(gross_sales) AS gross_sales FROM snitch_db.maplemonk.fact_items_snitch GROUP BY order_name, date ) SELECT date, COUNT(order_name) AS order_count FROM low_value GROUP BY date );",
+                                "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_high_discount AS ( WITH high_discount AS ( SELECT order_name, order_timestamp::date AS date, div0(SUM(discount), SUM(discount + gross_sales)) * 100 AS discount_percentage FROM snitch_db.maplemonk.fact_items_snitch GROUP BY order_name, date ) SELECT date,discount_percentage, COUNT(order_name) AS order_count FROM high_discount GROUP BY date,discount_percentage ); CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_superlow_value AS ( WITH low_value AS ( SELECT order_name, order_timestamp::date AS date, SUM(gross_sales) AS gross_sales FROM snitch_db.maplemonk.fact_items_snitch GROUP BY order_name, date ) SELECT date,gross_sales, COUNT(order_name) AS order_count FROM low_value GROUP BY date,gross_sales );",
                                 "transaction": true
                             }
                         ) }}
