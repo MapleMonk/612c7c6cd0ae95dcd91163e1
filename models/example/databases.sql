0a126f76dbedcb76fd5af299ab672a44d5406797
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-07-17 07:22:20 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+                        materialized='table',
+                            post_hook={
+                                "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_high_discount AS ( WITH high_discount AS ( SELECT order_name, order_timestamp::date AS date, div0(SUM(discount), SUM(discount + gross_sales)) * 100 AS discount_percentage FROM snitch_db.maplemonk.fact_items_snitch GROUP BY order_name, date ) SELECT date, COUNT(order_name) AS order_count FROM high_discount GROUP BY date ); CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_superlow_value AS ( WITH low_value AS ( SELECT order_name, order_timestamp::date AS date, SUM(gross_sales) AS gross_sales FROM snitch_db.maplemonk.fact_items_snitch GROUP BY order_name, date ) SELECT date, COUNT(order_name) AS order_count FROM low_value GROUP BY date );",
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
+
+                            select * from snitch_db.information_schema.databases
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
