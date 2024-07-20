e1c77604c304bca20e219bb3e83d08ad8bd88082
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-07-20 12:36:14 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,17 +1,17 @@
 {{ config(
-                        materialized='table',
-                            post_hook={
-                                "sql": "create or replace table SELECT_DB.MAPLEMONK.Shopify_UTM_Parameters_fact_item as select * from SELECT_DB.MAPLEMONK.Shopify_kyari_co_UTM_Parameters ;",
-                                "transaction": true
-                            }
-                        ) }}
-                        with sample_data as (
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table SELECT_DB.MAPLEMONK.Shopify_UTM_Parameters_fact_item as select * from SELECT_DB.MAPLEMONK.Shopify_kyari_co_UTM_Parameters ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
 
-                            select * from SELECT_DB.information_schema.databases
-                        ),
-                        
-                        final as (
-                            select * from sample_data
-                        )
-                        select * from final
-                        
+                select * from SELECT_DB.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
