21a8c0d1d9711878da8c2c926749dcd001556271
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-07-24 14:20:58 +0530

    added new script

--- models/example/deduping_generic_script.sql
+++ models/example/deduping_generic_script.sql
@@ -15,7 +15,21 @@ input_data as (
 
 ),
  new_updated as (
-   SELECT * FROM (
+   SELECT * FROM 
+   {% if var('partitionWithoutArrayObjects', None) is not none %}
+    (
+         SELECT *,row_number() over(
+            partition by {{ var('partitionWithoutArrayObjects') }}
+            order by 
+            {{var("cursor_feild")}} is null asc,
+            {{var("cursor_feild")}} desc,
+            _AIRBYTE_EMITTED_AT desc
+        ) AS ROW_NUMBER_1
+        FROM 
+
+   {% endif %}
+   
+   (
         select *, row_number() over(
             partition by {{var("partitionRows")}}
             order by 
@@ -25,5 +39,10 @@ input_data as (
         ) AS ROW_NUMBER
       FROM input_data
      ) WHERE ROW_NUMBER = 1
+
+    {% if var('partitionWithoutArrayObjects', None) is not none %}
+       ) where ROW_NUMBER_1 = 1
+   {% endif %}
+   
 )
 SELECT {{var("orignalField")}}  FROM new_updated
