480260f06ce801f56e3b2b4deda8356228124f2c
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-08-07 05:32:53 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.returns_dto AS WITH dto AS ( SELECT \"Warehouse Name\", \"Tracking No\", \"Sale Order Item Code\", \"Original Sale Order Code\", \"Courier Provider Name\", \"Channel Return Created Date\", \"Reverse Pickup Status\", \"Reverse Pickup Last Updated\", \"Reverse Pickup Created\", \"Return Delivery Date\", \"Putaway Code\", \"Putaway Status\", \"Putaway Created By\", \"Putaway Last Updated\", \"Reverse Pickup No\", \"Item Name\", \"QC Comment\", \"Return Reason\", \"Channel Name\", \"Item SkuCode\", \"Shipping Courier Status\", \"Shipping Tracking Status\", \"Reverse Pickup Created By\", \"Total Received Items\" FROM snitch_db.maplemonk.snitch_get_copy_of_reverse_pickup ), clickpost_db AS ( SELECT awb, \"RTO AWB\", \"RTO Mark Date\", \"Order ID\", \"Drop City\", \"Order Date\", \"Created at\", \"Pickup Date\", \"Delivery Date\", \"Drop Name\", \"Updated at\", \"Items Quantity\", \"RVP Reason\", \"Return Name\", \"Latest Timestamp\", \"Clickpost Unified Status\", \"Product SKU Code\", \"Courier Partner\" from( SELECT awb, \"RTO AWB\", \"RTO Mark Date\", \"Order ID\", \"Drop City\", \"Order Date\", \"Created at\", \"Pickup Date\", \"Delivery Date\", \"Drop Name\", \"Updated at\", \"Items Quantity\", \"RVP Reason\", \"Return Name\", \"Latest Timestamp\", \"Clickpost Unified Status\", \"Product SKU Code\", \"Courier Partner\", ROW_NUMBER() OVER (PARTITION BY awb ORDER BY \"Latest Timestamp\" DESC) AS row_num FROM snitch_db.maplemonk.snitch_clickpost_track_order_dashboard_report) where row_num = 1 ) SELECT dto.*, cp.*, ROW_NUMBER() OVER ( PARTITION BY dto.\"Sale Order Item Code\" ORDER BY COALESCE(cp.\"Latest Timestamp\", dto.\"Reverse Pickup Last Updated\") DESC ) AS row_num, CASE WHEN cp.\"Delivery Date\" IS NULL AND dto.\"Return Delivery Date\" IS NULL THEN NULL ELSE COALESCE(cp.\"Delivery Date\", dto.\"Return Delivery Date\") END AS Final_Delivery_date FROM dto LEFT JOIN clickpost_db cp ON dto.\"Tracking No\" = cp.awb",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from snitch_db.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
