9f5540965ffbf30d41684ed0517e17e6d1c3f9cd
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-08-12 19:45:03 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE OR REPLACE TABLE MAPLEMONK.ZOUK_MYNTRA_ADS_FACT_ITEMS AS select \'MYNTRA\' CHANNEL ,SAFE_CAST(CTR as FLOAT64) as CTR ,SAFE_CAST(CVR as FLOAT64) as CVR ,PARSE_DATE(\'%d-%b-%Y\', Date) Date ,SAFE_CAST(Views as FLOAT64) as Views ,SAFE_CAST(Clicks as FLOAT64) as Clicks ,Product_ID ,Ad_Group_ID ,Campaign_ID ,SAFE_CAST(Impressions as FLOAT64) as Impressions ,Product_Name ,SAFE_CAST(ROI__Direct_ as FLOAT64) as ROI_Direct ,Ad_Group_Name ,Campaign_Name ,SAFE_CAST(ROI__Indirect_ as FLOAT64) as ROI_Indirect ,SAFE_CAST(Units_Sold__Direct_ as FLOAT64) as Units_Sold_Direct ,SAFE_CAST(Units_Sold__InDirect_ as FLOAT64) as Units_Sold_InDirect ,SAFE_CAST(Advertiser_Spend_in_Currency__in_INR_ as FLOAT64) as Advertiser_Spend_in_Currency_in_INR ,SAFE_CAST(Revenue_in_Currency__Direct___in_INR_ as FLOAT64) as Revenue_in_Currency_Direct_in_INR ,SAFE_CAST(Revenue_in_Currency__Indirect___in_INR_ as FLOAT64) as Revenue_in_Currency_Indirect_in_INR ,_ab_source_file_url from `MapleMonk.ZOUK_MYNTRA_PRODUCT_ADS` where date <> \'\';",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
