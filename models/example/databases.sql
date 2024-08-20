9147450ed349dbaf77ff3d49fa43a975eea0d001
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-08-20 08:10:22 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,17 +1,17 @@
 {{ config(
-                        materialized='table',
-                            post_hook={
-                                "sql": "CREATE OR REPLACE TABLE SNITCH_DB.MAPLEMONK.LIVE_INV_WAREHOUSE_offline_replen AS SELECT sku, facility, SUM(units_on_hand) AS units_on_hand FROM ( SELECT \"Item Type skuCode\" AS sku, facility, COUNT(DISTINCT \"Item Code\") AS units_on_hand FROM SNITCH_DB.MAPLEMONK.unicommerce_inventory_aging WHERE CONVERT_TIMEZONE(\'UTC\', \'Asia/Kolkata\', _airbyte_emitted_at::DATETIME)::date = current_date() -1 GROUP BY 1, 2 ) AS subquery GROUP BY sku, facility UNION ALL SELECT \"Item SkuCode\" AS sku, FACILITY, COALESCE(SUM(inventory), 0) AS total_units_on_hand FROM snitch_db.maplemonk.snitch_final_inventory_wh2 WHERE date = current_date GROUP BY sku, FACILITY;",
-                                "transaction": true
-                            }
-                        ) }}
-                        with sample_data as (
+            materialized='table',
+                post_hook={
+                    "sql": "ALTER SESSION SET TIMEZONE = \'Asia/Kolkata\'; CREATE OR REPLACE TABLE SNITCH_DB.MAPLEMONK.LIVE_INV_WAREHOUSE_offline_replen AS SELECT \"Item SkuCode\" AS sku, FACILITY, COALESCE(SUM(inventory), 0) AS total_units_on_hand FROM snitch_db.maplemonk.snitch_final_inventory_wh2 WHERE date = current_date GROUP BY sku, FACILITY;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
 
-                            select * from snitch_db.information_schema.databases
-                        ),
-                        
-                        final as (
-                            select * from sample_data
-                        )
-                        select * from final
-                        
+                select * from snitch_db.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
