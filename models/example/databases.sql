c2fcf53e5fe6acdca0dd7ce00c3a32d069770b8a
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-09-09 13:35:11 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,17 +1,17 @@
 {{ config(
-                        materialized='table',
-                            post_hook={
-                                "sql": "create or replace table snitch_db.maplemonk.unicommerce_availability_merge as SELECT fa.*, sgt.sleeve_type, sgt.collar_type, sgt.fabric, sgt.design, sgt.hem, sgt.closure, sgt.fit, sgt.occassion, sgt.product_type, sgt.color FROM snitch_db.maplemonk.unicommerce_fact_items_snitch fa LEFT JOIN snitch_db.maplemonk.sku_group_tags sgt ON fa.sku_group = sgt.sku_group",
-                                "transaction": true
-                            }
-                        ) }}
-                        with sample_data as (
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table snitch_db.maplemonk.unicommerce_availability_merge as with metafields as ( select designs, collar_new, material_new, occassion_new, print_design, sleeve_type, fit, style, color, new_category, sku_group, from snitch_db.maplemonk.product_info where date_wise = \'2024-09-09\' order by sku_group ) SELECT fa.*, metafields.designs, metafields.collar_new, metafields.material_new, metafields.occassion_new, metafields.print_design, metafields.sleeve_type, metafields.fit, metafields.style, metafields.new_category, metafields.color FROM snitch_db.maplemonk.unicommerce_fact_items_snitch fa LEFT JOIN metafields ON fa.sku_group = metafields.sku_group",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
 
-                            select * from snitch_db.information_schema.databases
-                        ),
-                        
-                        final as (
-                            select * from sample_data
-                        )
-                        select * from final
-                        
+                select * from snitch_db.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
