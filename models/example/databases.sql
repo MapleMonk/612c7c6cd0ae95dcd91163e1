d18db411b2f59d5e25088c11d4ce947654c38328
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-09-10 16:33:00 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "Create or replace table maplemonk.zouk_db_target_vs_Achieved as With Sales as ( Select * ,case when CURRENT_DATE() > Month then 0 else DATE_DIFF(Month, CURRENT_DATE(), DAY)+1 end as days_Remaining ,EXTRACT(DAY FROM LAST_DAY(cast(Month as date))) as no_of_days ,safe_divide(sales,(EXTRACT(DAY FROM cast(Month as date)) -(case when CURRENT_DATE() >Month then 0 else DATE_DIFF(Month, CURRENT_DATE(), DAY) end) )-1) Avg_Sales from ( Select marketplace ,FINAL_MARKETPLACE ,MARKETPLACE_SEGMENT ,last_day(Order_Date) Month ,sum(ifnull(SELLING_PRICE,0)) Sales from maplemonk.zouk_sales_consolidated group by 1,2,3,4 ) ), Targets as ( select marketplace ,last_day(PARSE_DATE(\'%b/%Y\', month)) Month ,cast(replace(target,\',\',\'\') as int64) Targets from MapleMonk.zouk_db_Metrics_Targets ) select s.*,T.Targets from Sales S left join Targets T on lower(s.marketplace) = lower(T.marketplace) and s.month = t.month",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
