4dec47152aba708dbf3ffb159db006edc8a1854f
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-09-10 13:02:26 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE OR REPLACE TABLE VAHDAM_DB.MAPLEMONK.Marketing_Consolidated_MX AS Select upper(adGroupName) ADSET_NAME ,ADID ::varchar as ADSET_ID ,null as ACCOUNT_NAME ,null as ACCOUNT_ID ,upper(CAMPAIGNNAME) as CAMPAIGN_NAME ,CAMPAIGNID ::varchar as CAMPAIGN_ID ,DATE ::DATE DATE ,CAMPAIGN_TYPE AD_TYPE ,NULL AS AD_STRENGTH ,NULL AS AD_NETWORK_TYPE ,NULL AS AD_URL ,dayname(try_to_date(date)) DAY_OF_WEEK ,year(try_to_date(date)) YEAR ,month(try_to_date(date)) MONTH ,\'AMAZON\' AS CHANNEL ,adGroupId::varchar AS ACCOUNT ,sum(ifnull(CLICKS,0)) CLICKS ,sum(ifnull(CLICKS,0)) OUTBOUND_CLICKS ,sum(ifnull(SPEND,0)) as SPEND ,sum(ifnull(IMPRESSIONS,0))IMPRESSIONS ,sum(ifnull(CONVERSIONS,0)) CONVERSIONS ,sum(ifnull(SALES,0)) CONVERSION_VALUE from VAHDAM_DB.MAPLEMONK.AMAZONADS_MX_MARKETING group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from VAHDAM_DB.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
