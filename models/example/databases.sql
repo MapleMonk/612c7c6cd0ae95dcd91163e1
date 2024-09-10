a352d914412b24c70458eaedd546edf76c4b7441
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-09-10 07:52:55 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "ALTER SESSION SET TIMEZONE = \'Asia/Kolkata\'; CREATE OR REPLACE TABLE SNITCH_DB.MAPLEMONK.LIVE_INV_WAREHOUSE_offline_replen AS SELECT \"Item SkuCode\" AS sku, FACILITY, COALESCE(SUM(inventory), 0) AS units_on_hand FROM snitch_db.maplemonk.snitch_final_inventory_wh2 WHERE date = current_date GROUP BY sku, FACILITY;",
+                    "sql": "ALTER SESSION SET TIMEZONE = \'Asia/Kolkata\'; CREATE OR REPLACE TABLE SNITCH_DB.MAPLEMONK.LIVE_INV_WAREHOUSE_offline_replen AS SELECT \"Item SkuCode\" AS sku, FACILITY, COALESCE(SUM(inventory), 0) AS units_on_hand FROM snitch_db.maplemonk.snitch_final_inventory_wh2 WHERE date = current_date AND FACILITY !=\'SAPL-EMIZA\' GROUP BY sku, FACILITY;",
                     "transaction": true
                 }
             ) }}
