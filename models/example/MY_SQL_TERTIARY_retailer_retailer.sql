2f517dc1c9474e4b74ae46b26d21d0b6f769f691
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-09-16 09:56:31 +0000

    model file has updated

--- models/example/MY_SQL_TERTIARY_retailer_retailer.sql
+++ models/example/MY_SQL_TERTIARY_retailer_retailer.sql
@@ -1,17 +1,17 @@
 {{ config(
-                        materialized='table',
-                            post_hook={
-                                "sql": "create or replace table eggozdb.maplemonk.my_sql_tertiary_retailer_retailer as select rr.*,r1.code as dealer_name, r2.code as distributor_name from eggozdb.maplemonk.my_sql_tertiary_retailer_retailer rr left join eggozdb.maplemonk.my_sql_retailer_retailer r1 on rr.dealer_id = r1.id left join eggozdb.maplemonk.my_sql_retailer_retailer r2 on r1.distributor_id = r2.id left join eggozdb.maplemonk.my_sql_retailer_customer_category rcc on rcc.id = rr.category_id left join eggozdb.maplemonk.my_sql_retailer_customer_subcategory rcs on rcs.id = rr.sub_category_id where rr.dealer_id is not null ;",
-                                "transaction": true
-                            }
-                        ) }}
-                        with sample_data as (
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table eggozdb.maplemonk.my_sql_tertiary_retailer_retailer as select rr.*,r1.code as dealer_name, r2.code as distributor_name from eggozdb.maplemonk.my_sql_tertiary_retailer_retailer rr left join eggozdb.maplemonk.my_sql_retailer_retailer r1 on rr.dealer_id = r1.id left join eggozdb.maplemonk.my_sql_retailer_retailer r2 on r1.distributor_id = r2.id left join eggozdb.maplemonk.my_sql_retailer_customer_category rcc on rcc.id = rr.category_id left join eggozdb.maplemonk.my_sql_retailer_customer_subcategory rcs on rcs.id = rr.sub_category_id where rr.dealer_id is not null and rr.category_id = 11 ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
 
-                            select * from EGGOZDB.MAPLEMONK.MY_SQL_TERTIARY_retailer_retailer
-                        ),
-                        
-                        final as (
-                            select * from sample_data
-                        )
-                        select * from final
-                        
+                select * from EGGOZDB.MAPLEMONK.MY_SQL_TERTIARY_retailer_retailer
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
