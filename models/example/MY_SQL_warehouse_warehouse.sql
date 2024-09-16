6359075f83648e488382682e4063e00a41f1d650
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-09-16 08:40:10 +0000

    model file has updated

--- models/example/MY_SQL_warehouse_warehouse.sql
+++ models/example/MY_SQL_warehouse_warehouse.sql
@@ -1,17 +1,17 @@
 {{ config(
-                        materialized='table',
-                            post_hook={
-                                "sql": "CREATE OR REPLACE TABLE eggozdb.maplemonk.my_sql_product_product AS SELECT *, CASE WHEN brand_type = \'branded\' THEN CASE WHEN short_name = \'WE\' THEN \'Nogyo\' WHEN short_name = \'WEE\' THEN \'Everyday\' WHEN short_name = \'WEENECC\' THEN \'Everyday\' WHEN short_name = \'WD\' THEN \'Darjan\' WHEN short_name = \'FR\' THEN \'Free Range\' WHEN short_name = \'CH\' THEN \'Champs\' WHEN short_name = \'JP\' THEN \'Champs\' WHEN short_name = \'W\' THEN \'Eggoz Premium\' WHEN short_name = \'B\' THEN \'Eggoz Premium\' WHEN short_name = \'N\' THEN \'Eggoz Premium\' WHEN short_name like \'%*%\' THEN \'Eggoz Premium\' WHEN productSubDivision_id = 42 THEN \'Frozen\' WHEN productSubDivision_id = 45 THEN \'Eazy Eggs\' ELSE \'Branded Others\' END WHEN brand_type = \'unbranded\' THEN CASE WHEN short_name = \'L\' then \'Unbranded Liquid\' ELSE \'Unbranded Others\' END END as product_type FROM eggozdb.maplemonk.my_sql_product_product ;",
-                                "transaction": true
-                            }
-                        ) }}
-                        with sample_data as (
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE OR REPLACE TABLE eggozdb.maplemonk.my_sql_product_product AS SELECT *, CASE WHEN brand_type = \'branded\' THEN CASE WHEN short_name = \'WE\' THEN \'Nogyo\' WHEN short_name = \'WEE\' THEN \'Everyday\' WHEN short_name = \'WEENECC\' THEN \'Everyday\' WHEN short_name = \'BEE\' THEN \'Everyday\' WHEN short_name = \'FWE\' THEN \'Everyday\' WHEN short_name = \'WD\' THEN \'Darjan\' WHEN short_name = \'FR\' THEN \'Free Range\' WHEN short_name = \'CH\' THEN \'Champs\' WHEN short_name = \'JP\' THEN \'Champs\' WHEN short_name = \'W\' THEN \'Eggoz Premium\' WHEN short_name = \'B\' THEN \'Eggoz Premium\' WHEN short_name = \'N\' THEN \'Eggoz Premium\' WHEN short_name like \'%*%\' THEN \'Eggoz Premium\' WHEN productSubDivision_id = 42 THEN \'Frozen\' WHEN productSubDivision_id = 45 THEN \'Eazy Eggs\' ELSE \'Branded Others\' END WHEN brand_type = \'unbranded\' THEN CASE WHEN short_name = \'L\' then \'Unbranded Liquid\' ELSE \'Unbranded Others\' END END as product_type FROM eggozdb.maplemonk.my_sql_product_product ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
 
-                            select * from EGGOZDB.MAPLEMONK.MY_SQL_warehouse_warehouse
-                        ),
-                        
-                        final as (
-                            select * from sample_data
-                        )
-                        select * from final
-                        
+                select * from EGGOZDB.MAPLEMONK.MY_SQL_warehouse_warehouse
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
