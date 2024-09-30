9c8e97427686829d0b44a2882e26e378bcaa2ef0
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-09-30 05:53:17 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table snitch_db.maplemonk.total_sales_dod as ( select date, \'Shopify\' as type, sum(gross_sales) as total_sales from snitch_db.maplemonk.sales_cost_source_snitch group by 1,2 union select order_date as date, marketplace_mapped as type, sum(case when lower(marketplace_mapped) = \'ajio\' then selling_price*1.45 else selling_price end) as total_sales from snitch_db.maplemonk.unicommerce_fact_items_snitch where marketplace_mapped IN (\'Myntra\',\'SNAPMINT\',\'CRED\',\'DONOSHOP\',\'FYND\',\'AMAZON\',\'MYNTRA\',\'MENSXP\',\'NYKAA_FASHION\',\'AJIO\',\'FLIPKART\') group by 1,2 union select date as date, marketplace_mapped as type, sum(today_sales) as total_sales from snitch_db.maplemonk.offline_detailed_summary group by 1,2 );",
+                    "sql": "create or replace table snitch_db.maplemonk.total_sales_dod as ( select date, \'Shopify\' as type, sum(gross_sales) as total_sales from snitch_db.maplemonk.sales_cost_source_snitch group by 1,2 union select order_date as date, marketplace_mapped as type, sum(case when lower(marketplace_mapped) = \'ajio\' then selling_price*1.45 else selling_price end) as total_sales from snitch_db.maplemonk.unicommerce_fact_items_snitch where marketplace_mapped IN (\'Myntra\',\'SNAPMINT\',\'CRED\',\'DONOSHOP\',\'FYND\',\'AMAZON\',\'MYNTRA\',\'MENSXP\',\'NYKAA_FASHION\',\'AJIO\',\'FLIPKART\') group by 1,2 union select date as date, marketplace_mapped as type, sum(today_sales) as total_sales from snitch_db.maplemonk.offline_detailed_summary where marketplace_mapped not like \'%WH%\' group by 1,2 );",
                     "transaction": true
                 }
             ) }}
