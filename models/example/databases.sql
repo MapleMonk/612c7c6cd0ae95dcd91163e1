dc0415815fc637c849f2cf1559cb3e7a9b4111e3
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-10-09 08:27:40 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE MAPLEMONK.ZOUK_SWIGGY_ADS_FACT_ITEMS AS select \'SWIGGY\' CHANNEL ,\'PRODUCT ADS\' AD_TYPE ,SAFE_CAST(TOTAL_CTR as FLOAT64) as CTR ,cast(COALESCE(SAFE.PARSE_DATE(\'%d-%m-%Y\', METRICS_DATE),SAFE.PARSE_DATE(\'%d/%m/%Y\', METRICS_DATE)) as date) Date ,SAFE_CAST(0 as FLOAT64) as Views ,SAFE_CAST(TOTAL_CLICKS as FLOAT64) as Clicks ,Campaign_ID ,SAFE_CAST(TOTAL_IMPRESSIONS as FLOAT64) as Impressions ,Product_Name ,Campaign_Name ,SAFE_CAST(TOTAL_ROI as FLOAT64) * SAFE_CAST(TOTAL_BUDGET_BURNT as FLOAT64) as ad_sales ,SAFE_CAST(TOTAL_BUDGET_BURNT as FLOAT64) as spend from `MapleMonk.ZOUK_S3_SWIGGY_INSTAMART_ADS` where METRICS_DATE <> \'\';",
+                    "sql": "CREATE OR REPLACE TABLE MAPLEMONK.ZOUK_SWIGGY_ADS_FACT_ITEMS AS select \'SWIGGY\' CHANNEL ,\'PRODUCT ADS\' AD_TYPE ,SAFE_CAST(TOTAL_CTR as FLOAT64) as CTR ,COALESCE(SAFE.PARSE_DATE(\'%d-%b-%Y\', METRICS_DATE),SAFE.PARSE_DATE(\'%d/%m/%y\', METRICS_DATE),SAFE.PARSE_DATE(\'%d-%m-%y\', METRICS_DATE)) AS Date ,SAFE_CAST(0 as FLOAT64) as Views ,SAFE_CAST(TOTAL_CLICKS as FLOAT64) as Clicks ,Campaign_ID ,SAFE_CAST(TOTAL_IMPRESSIONS as FLOAT64) as Impressions ,Product_Name ,Campaign_Name ,SAFE_CAST(TOTAL_ROI as FLOAT64) * SAFE_CAST(TOTAL_BUDGET_BURNT as FLOAT64) as ad_sales ,SAFE_CAST(TOTAL_BUDGET_BURNT as FLOAT64) as spend from `MapleMonk.ZOUK_S3_SWIGGY_INSTAMART_ADS` where METRICS_DATE <> \'\';",
                     "transaction": true
                 }
             ) }}
