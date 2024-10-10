ae1ce82e959a83f9a9966213cbbf6c34cd0d1dfa
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-10-10 10:18:24 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table snitch_db.maplemonk.logistics_convertor as SELECT replace(f.value,\' \',\'\') as ind_so, t.\"DATE\", t.remark, t.no_boxes, t.dc_number, t.lr_number, t.adj_logic, t.d_quantity, t.excess_qty, t.store_name, t.inward_date, t.qty_received, t.shortage_qty, t.logic_ref_num, t.store_remarks, t.boxes_received, t.order_received_date, t.so_number FROM snitch_db.maplemonk.gs_outstation_dispatch t, LATERAL FLATTEN(input => SPLIT(t.so_number, \'\n\')) f",
+                    "sql": "create or replace table snitch_db.maplemonk.logistics_convertor as SELECT trim(f.value) as ind_so, t.\"DATE\", t.remark, t.no_boxes, t.dc_number, t.lr_number, t.adj_logic, t.d_quantity, t.excess_qty, t.store_name, t.inward_date, t.qty_received, t.shortage_qty, t.logic_ref_num, t.store_remarks, t.boxes_received, t.order_received_date, t.so_number FROM snitch_db.maplemonk.gs_outstation_dispatch t, LATERAL FLATTEN(input => SPLIT( REGEXP_REPLACE(t.so_number, \'\\r\\n|\\n\', \',\'), \',\' )) f",
                     "transaction": true
                 }
             ) }}
