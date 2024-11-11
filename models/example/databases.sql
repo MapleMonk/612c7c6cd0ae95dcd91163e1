fefa336343515cec019d97c3212be4fc6061a3c9
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-11-11 07:51:58 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table snitch_db.maplemonk.banner_funnel_data as ( select to_date(date,\'YYYYMMDD\') as ga_date, \'Web\' as Type, \'None\' as operating_system, itempromotionname as banner_name, itemsviewedinpromotion as views, itemsclickedinpromotion as clicks, from snitch_db.maplemonk.WEB_BANNER_VIEWS_CLICKS where banner_name != \'(not set)\' union select to_date(date,\'YYYYMMDD\') as ga_date, \'App\' as Type, operatingsystem as operating_system, itempromotionname as banner_name, itemsviewedinpromotion as views, itemsclickedinpromotion as clicks, from snitch_db.maplemonk.app_banner_clicks where banner_name != \'(not set)\' )",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from snitch_db.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
