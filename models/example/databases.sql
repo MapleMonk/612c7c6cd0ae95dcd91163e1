d40f255c5987f99a70fc21a0e420b8ef14b2f65a
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-11-21 08:48:37 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table snitch_db.maplemonk.banner_funnel_data as ( select to_date(date,\'YYYYMMDD\') as ga_date, \'Web\' as Type, \'None\' as operating_system, itempromotionname as banner_name, itemsviewedinpromotion as views, itemsclickedinpromotion as clicks, itempromotionclickthroughrate as ctr from snitch_db.maplemonk.WEB_BANNER_VIEWS_CLICKS where banner_name != \'(not set)\' union select to_date(date,\'YYYYMMDD\') as ga_date, \'App\' as Type, operatingsystem as operating_system, itempromotionname as banner_name, itemsviewedinpromotion as views, itemsclickedinpromotion as clicks, itempromotionclickthroughrate as ctr from snitch_db.maplemonk.ga_app_banner_clicks where banner_name != \'(not set)\' )",
+                    "sql": "create or replace table snitch_db.maplemonk.banner_funnel_data as ( select to_date(date,\'YYYYMMDD\') as ga_date, \'Web\' as Channel, \'None\' as operating_system, itempromotionname as banner_name, itemsviewedinpromotion as views, itemsclickedinpromotion as clicks, itempromotionclickthroughrate as ctr from snitch_db.maplemonk.WEB_BANNER_VIEWS_CLICKS where banner_name != \'(not set)\' union select to_date(date,\'YYYYMMDD\') as ga_date, \'App\' as Channel, operatingsystem as operating_system, itempromotionname as banner_name, itemsviewedinpromotion as views, itemsclickedinpromotion as clicks, itempromotionclickthroughrate as ctr from snitch_db.maplemonk.ga_app_banner_clicks where banner_name != \'(not set)\' );",
                     "transaction": true
                 }
             ) }}
