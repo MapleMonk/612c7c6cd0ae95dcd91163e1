1531275687c59a9b20e76114378e812953e9abca
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-11-23 06:02:55 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE `maplemonk.zouk_grand_vegan_sale` AS SELECT Order_Date, Marketplace, CASE WHEN COUNT(DISTINCT Order_ID) > 0 THEN COUNT(DISTINCT Order_ID) ELSE ROUND(SUM(Quantity) / 1.5) END AS Order_Count, SUM(Quantity) AS Total_Quantity FROM `maplemonk.zouk_secondary_sales_consolidated` WHERE Order_Date BETWEEN DATE(\'2024-11-01\') AND DATE(\'2024-12-03\') GROUP BY Order_Date, Marketplace ORDER BY Order_Date, Marketplace;",
+                    "sql": "CREATE OR REPLACE TABLE `maplemonk.zouk_grand_vegan_sale` AS SELECT Order_Date, Marketplace, CASE WHEN COUNT(DISTINCT Order_ID) > 0 THEN COUNT(DISTINCT Order_ID) ELSE ROUND(SUM(Quantity)/1.2) END AS Order_Count, SUM(Quantity) AS Total_Quantity FROM `maplemonk.zouk_secondary_sales_consolidated` WHERE Order_Date BETWEEN DATE(\'2024-11-01\') AND DATE(\'2024-12-03\') GROUP BY Order_Date, Marketplace ORDER BY Order_Date, Marketplace;",
                     "transaction": true
                 }
             ) }}
