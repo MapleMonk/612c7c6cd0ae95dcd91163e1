f036a70660c37c717e241875092808882c42571c
Naveen Patnana <naveen_patnana@maplemonk.com>
2024-11-26 11:00:47 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,17 +1,17 @@
 {{ config(
-                        materialized='table',
-                            post_hook={
-                                "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_high_discount_low_value AS ( SELECT order_name, order_timestamp::date AS date, discount_code, sum(quantity) as quantity, sum(discount) as discount, SUM(gross_sales) AS gross_sales, div0(SUM(discount), SUM(discount + gross_sales)) * 100 AS discount_percentage FROM snitch_db.maplemonk.fact_items_snitch WHERE lower(ifnull(discount_code,\'n\')) not like \'%eco%\' and lower(ifnull(discount_code,\'n\')) not like \'%influ%\' and order_name not in (\'2431093\',\'2422140\',\'2425364\',\'2430652\',\'2422237\',\'2420623\',\'2429832\',\'2422378\',\'2428311\',\'2429064\',\'2428204\',\'2421343\',\'2431206\',\'2430491\',\'2426682\',\'2426487\',\'2426458\',\'2423575\',\'2422431\',\'2423612\',\'2426625\',\'2428117\',\'2426894\',\'2425461\',\'2426570\',\'2423455\',\'2430777\',\'2426009\',\'2428245\',\'2427269\',\'2430946\',\'2425821\',\'2429986\',\'2429085\',\'2422047\',\'2430789\',\'2420219\',\'2428341\',\'2430444\',\'2426866\',\'2431230\',\'2425839\',\'2430980\',\'2427048\',\'2430597\',\'2420499\',\'2431050\',\'2420271\',\'2426684\',\'2428747\',\'2423523\',\'2431171\',\'2430830\',\'2425325\',\'2428414\',\'2429054\',\'2423596\') and tags not in (\'FLITS_LOGICERP\') and lower(order_status) != \'cancelled\' GROUP BY order_name, date, discount_code );",
-                                "transaction": true
-                            }
-                        ) }}
-                        with sample_data as (
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.datewise_high_discount_low_value AS ( SELECT order_name, order_timestamp::date AS date, discount_code, sum(quantity) as quantity, sum(slashed_discount) as slashed_discount, sum(discount) as discount, sum(coupon_discount) as coupon_discount, SUM(gross_sales) AS gross_sales, div0(SUM(discount), SUM(discount + gross_sales)) * 100 AS discount_percentage FROM select * from snitch_db.maplemonk.fact_items_snitch limit 10 WHERE lower(ifnull(discount_code,\'n\')) not like \'%eco%\' and lower(ifnull(discount_code,\'n\')) not like \'%influ%\' and order_name not in (\'2431093\',\'2422140\',\'2425364\',\'2430652\',\'2422237\',\'2420623\',\'2429832\',\'2422378\',\'2428311\',\'2429064\',\'2428204\',\'2421343\',\'2431206\',\'2430491\',\'2426682\',\'2426487\',\'2426458\',\'2423575\',\'2422431\',\'2423612\',\'2426625\',\'2428117\',\'2426894\',\'2425461\',\'2426570\',\'2423455\',\'2430777\',\'2426009\',\'2428245\',\'2427269\',\'2430946\',\'2425821\',\'2429986\',\'2429085\',\'2422047\',\'2430789\',\'2420219\',\'2428341\',\'2430444\',\'2426866\',\'2431230\',\'2425839\',\'2430980\',\'2427048\',\'2430597\',\'2420499\',\'2431050\',\'2420271\',\'2426684\',\'2428747\',\'2423523\',\'2431171\',\'2430830\',\'2425325\',\'2428414\',\'2429054\',\'2423596\') and tags not in (\'FLITS_LOGICERP\') and lower(order_status) != \'cancelled\' GROUP BY order_name, date, discount_code );",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
 
-                            select * from snitch_db.information_schema.databases
-                        ),
-                        
-                        final as (
-                            select * from sample_data
-                        )
-                        select * from final
-                        
+                select * from snitch_db.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
