7577c26ebe87636b23e2867f95f04e27119ec90e
MapleMonk-Dev <development@maplemonk.com>
2025-02-26 14:19:25 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table ras-wh.maplemonk.ras_inventory_fact_items as select upper(productSKU) SKU ,coalesce(sm.product_name, upper(productName)) Product_Name ,sm.brand ,sm.category ,sm.mrp ,upper(batch) Batch ,upper(binName) binName ,cast(shelfLife as int64) Shelf_Life ,parse_date(\'%d-%m-%Y\',manufacturingDate) Manufacturing_date ,parse_date(\'%d-%m-%Y\',expiryDate) Expiry_Date ,DATE_DIFF( PARSE_DATE(\'%d-%m-%Y\', expiryDate), PARSE_DATE(\'%d-%m-%Y\', manufacturingDate), DAY ) AS Estimated_Shelf_Life_Days ,DATE_DIFF( PARSE_DATE(\'%d-%m-%Y\', expiryDate), PARSE_DATE(\'%d-%m-%Y\', manufacturingDate), MONTH ) AS Estimated_Shelf_Life_Months ,DATE_DIFF( PARSE_DATE(\'%d-%m-%Y\', expiryDate), CURRENT_DATE(\'Asia/Kolkata\'), DAY ) AS Remaining_Life_Days ,DATE_DIFF( PARSE_DATE(\'%d-%m-%Y\', expiryDate), CURRENT_DATE(\'Asia/Kolkata\'), MONTH ) AS Remaining_Life_Months ,case when DATE_DIFF( PARSE_DATE(\'%d-%m-%Y\', expiryDate), CURRENT_DATE(\'Asia/Kolkata\'), DAY ) <=0 then \'EXPIRED\' else \'ACTIVE\' end Expiry_Status ,cast(availableQuantity as int64) Available_Quantity from `MAPLEMONK.edgistify_get_master_inventory_details` id left join ras-wh.maplemonk.ras_sku_master sm on upper(id.productSKU) = sm.sku ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
