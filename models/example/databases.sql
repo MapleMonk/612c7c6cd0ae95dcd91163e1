1a588f1ddb8dc12dffd3ef2186ff9875ae3fab6a
MapleMonk-Dev <development@maplemonk.com>
2025-02-27 14:12:39 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.offline_footfall_delopt AS SELECT DATE_TRUNC(\'minute\', CASE WHEN COUNT__DATETIME LIKE \'__-__-____ __:__\' THEN TO_TIMESTAMP(COUNT__DATETIME, \'MM-DD-YYYY HH24:MI\') WHEN COUNT__DATETIME LIKE \'__/__/____ __:__\' THEN TO_TIMESTAMP(COUNT__DATETIME, \'MM/DD/YYYY HH24:MI\') WHEN COUNT__DATETIME LIKE \'_/__/____ __:__\' THEN TO_TIMESTAMP(COUNT__DATETIME, \'MM/DD/YYYY HH24:MI\') ELSE NULL END ) AS CUSTOM_DATE ,* from snitch_db.maplemonk.offline_footfall_delopt",
+                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.offline_footfall_delopt1 AS SELECT DATE_TRUNC(\'minute\', CASE WHEN COUNT__DATETIME LIKE \'__-__-____ __:__\' THEN TO_TIMESTAMP(COUNT__DATETIME, \'MM-DD-YYYY HH24:MI\') WHEN COUNT__DATETIME LIKE \'__/__/____ __:__\' THEN TO_TIMESTAMP(COUNT__DATETIME, \'MM/DD/YYYY HH24:MI\') WHEN COUNT__DATETIME LIKE \'_/__/____ __:__\' THEN TO_TIMESTAMP(COUNT__DATETIME, \'MM/DD/YYYY HH24:MI\') ELSE NULL END ) AS CUSTOM_DATE ,* from snitch_db.maplemonk.offline_footfall_delopt",
                     "transaction": true
                 }
             ) }}
