269104f109776f0ed9dfa1c191881b98f46d1ba0
MapleMonk-Dev <development@maplemonk.com>
2025-03-11 11:10:00 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "Create or replace table RPSG_DB.MAPLEMONK.UTM_DATA as select order_date,reference_code,final_source,final_medium ,final_utm_campaign,ga_campaign,shopify_utm_campaign from RPSG_DB.MAPLEMONK.SALES_CONSOLIDATED_DRV WHERE marketplace IN (\'SHOPIFY_DRV\', \'SHOPIFY_AYURVEDICSOURCE\', \'SHOPIFY_HERBOBUILD\', \'Woocommerce\', \'Woocommerce 2\', \'Woocommerce 3\')",
+                    "sql": "Create or replace table RPSG_DB.MAPLEMONK.UTM_DATA as SELECT scd.ORDER_DATE, scd.FINAL_CHANNEL, scd.REFERENCE_CODE, scd.FINAL_SOURCE, scd.FINAL_MEDIUM, scd.FINAL_UTM_CAMPAIGN, scd.GA_CAMPAIGN, scd.SHOPIFY_UTM_CAMPAIGN, fisd.GOKWIK_UTM_CONTENT FROM RPSG_DB.MAPLEMONK.SALES_CONSOLIDATED_DRV scd LEFT JOIN RPSG_DB.MAPLEMONK.FACT_ITEMS_SHOPIFY_DRV fisd ON scd.REFERENCE_CODE = fisd.ORDER_NAME WHERE scd.MARKETPLACE IN (\'SHOPIFY_DRV\', \'SHOPIFY_AYURVEDICSOURCE\', \'SHOPIFY_HERBOBUILD\', \'Woocommerce\', \'Woocommerce 2\', \'Woocommerce 3\') and order_date >= \'2025-02-01\'",
                     "transaction": true
                 }
             ) }}
