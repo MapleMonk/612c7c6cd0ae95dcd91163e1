446a996a32414b09ebc098f5596361bcb39b712d
MapleMonk-Dev <development@maplemonk.com>
2025-03-12 03:57:30 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "Create or replace table maplemonk.Zouk_Product_Sales_Cost_Source as select Date, MARKETPLACE, COLLECTION, PRODUCT_CATEGORY, FINAL_MARKETPLACE, channel, new_customer_flag, customer_id_final, SOURCE, TRADE_MARGIN, REFERENCE_CODE, SKU, COMMONSKU, PRODUCT_TYPE, PRINT, category_code, BAU_MRP_SALES, BAU_DISCOUNT, SALES, Spend, Brand_Spend, return_mrp_sales, return_trade_margin, Returns, gst, Return_GST from maplemonk.zouk_pandl",
+                    "sql": "Create or replace table maplemonk.Zouk_Product_Sales_Cost_Source as select Date, MARKETPLACE, COLLECTION, PRODUCT_CATEGORY, FINAL_MARKETPLACE, channel, new_customer_flag, customer_id_final, SOURCE, TRADE_MARGIN, REFERENCE_CODE, SKU, COMMONSKU, PRODUCT_TYPE, PRINT, category_code, BAU_MRP_SALES, BAU_DISCOUNT, SALES, Spend, Brand_Spend, return_mrp_sales, return_trade_margin, Returns, gst, Return_GST, quantity from maplemonk.zouk_pandl",
                     "transaction": true
                 }
             ) }}
