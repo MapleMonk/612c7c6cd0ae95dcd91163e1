bf2f66641f7a8e684ce68222d523f16ad846c83c
MapleMonk-Dev <development@maplemonk.com>
2025-03-16 21:01:50 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table hox_db.maplemonk.HOX_YFL_7chef_tax_summary as select distinct a.*, b.packaging_box, div0(b.packaging_cost, count(1) over (partition by a.Reference_Code, lower(a.REPORT_TYPE), lower(a.MARKETPLACE), lower(a.sku) order by 1)) as PACKAGING_COST from hox_db.maplemonk.HOX_yfl_sevenchef_Tax_consolidated_report_intermediate a left join hox_db.maplemonk.HOX_YFL_packaging_material b on a.REFERENCE_CODE = b.REFERENCE_CODE and lower(a.marketplace) = lower(b.marketplace) and lower(a.report_type) = lower(b.report_type) and lower(a.sku) = lower(b.sku)",
+                    "sql": "create or replace table hox_db.maplemonk.HOX_YFL_7chef_tax_summary as select distinct a.*, b.packaging_box, div0(b.packaging_cost, count(1) over (partition by a.Reference_Code, lower(a.REPORT_TYPE), lower(a.MARKETPLACE), lower(a.sku) order by 1)) as PACKAGING_COST, case when lower(a.marketplace) like \'%_stn\' then \'stn\' when lower(a.marketplace) like any (\'%promotional%\', \'redispatch\', \'retail customers\') then \'no sale\' when lower(a.marketplace) like any (\'redispatch_cogs\' , \'shopify sp cogs\') then \'only cogs no sale\' else \'sales\' end as sales_type from hox_db.maplemonk.HOX_yfl_sevenchef_Tax_consolidated_report_intermediate a left join hox_db.maplemonk.HOX_YFL_packaging_material b on a.REFERENCE_CODE = b.REFERENCE_CODE and lower(a.marketplace) = lower(b.marketplace) and lower(a.report_type) = lower(b.report_type) and lower(a.sku) = lower(b.sku)",
                     "transaction": true
                 }
             ) }}
