9a2cc8cb458494c891065c6906ccd7c5202f63ce
MapleMonk-Dev <development@maplemonk.com>
2025-03-19 05:24:51 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE maplemonk.Zouk_Product_Secondary_Sales_Cost_Source AS SELECT zp.Date, zp.MARKETPLACE, zp.COLLECTION, zp.PRODUCT_CATEGORY, zp.FINAL_MARKETPLACE, nm.channel, zp.SOURCE, zp.TRADE_MARGIN, zp.REFERENCE_CODE, zp.SKU, zp.COMMONSKU, zp.PRODUCT_SUB_CATEGORY, zp.PRINT, zp.category_code, zp.BAU_MRP_SALES, zp.BAU_DISCOUNT, zp.SALES, zp.Spend, zp.Brand_Spend, zp.quantity FROM maplemonk.zouk_secondary_pandl zp LEFT JOIN maplemonk.zouk_New_Marketplace_Mapping nm ON LOWER(zp.MARKETPLACE) = LOWER(nm.marketplace);",
+                    "sql": "CREATE OR REPLACE TABLE maplemonk.Zouk_Product_Secondary_Sales_Cost_Source AS SELECT zp.Date, zp.MARKETPLACE, zp.COLLECTION, zp.PRODUCT_CATEGORY, zp.FINAL_MARKETPLACE, zp.Marketplace_Segment, nm.channel, zp.SOURCE, zp.TRADE_MARGIN, zp.REFERENCE_CODE, zp.SKU, zp.COMMONSKU, zp.PRODUCT_SUB_CATEGORY, zp.PRINT, zp.category_code, zp.BAU_MRP_SALES, zp.BAU_DISCOUNT, zp.SALES, zp.Spend, zp.Brand_Spend, zp.quantity FROM maplemonk.zouk_secondary_pandl zp LEFT JOIN maplemonk.zouk_New_Marketplace_Mapping nm ON LOWER(zp.MARKETPLACE) = LOWER(nm.marketplace);",
                     "transaction": true
                 }
             ) }}
