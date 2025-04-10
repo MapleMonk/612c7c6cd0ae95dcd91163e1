0ca31a3fcb70236b61449b8cd60e6451ef299bc0
MapleMonk-Dev <development@maplemonk.com>
2025-04-10 17:12:50 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE MapleMonk.Zouk_App_Performance_Update AS SELECT SAFE.PARSE_DATE(\'%Y-%m-%d\',Start_Date) AS Start_Date, SAFE.PARSE_DATE(\'%Y-%m-%d\',End_Date) AS End_Date, CAST(Total_Orders AS INT64) AS Total_Orders, CAST(REPLACE(AOV, \',\', \'\') AS FLOAT64) AS AOV, CAST(REPLACE(Total_Revenue, \',\', \'\') AS FLOAT64) AS Total_Revenue, CAST(REPLACE(Conversion_Rate, \'%\',\'\')AS FLOAT64) AS Conversion_Rate, CAST(REPLACE(Total_Downloads, \',\', \'\') AS FLOAT64) AS Total_Downloads, CAST(REPLACE(Total_New_Download, \',\', \'\') AS FLOAT64) AS Total_New_Download, CAST(REPLACE(Total_Spends_meta, \',\', \'\') AS FLOAT64) AS Total_Spends_meta, CAST(REPLACE(Cost_per_New_Download, \',\', \'\') AS FLOAT64) AS Cost_per_New_Download FROM MapleMonk.Zouk_App_Performance ;",
+                    "sql": "CREATE OR REPLACE TABLE MapleMonk.Zouk_App_Performance_Update AS SELECT SAFE.PARSE_DATE(\'%Y-%m-%d\',Start_Date) AS Start_Date, SAFE.PARSE_DATE(\'%Y-%m-%d\',End_Date) AS End_Date, CAST(Total_Orders AS INT64) AS Total_Orders, CAST(REPLACE(AOV, \',\', \'\') AS FLOAT64) AS AOV, CAST(REPLACE(Total_Revenue, \',\', \'\') AS FLOAT64) AS Total_Revenue, CAST(REPLACE(Conversion_Rate, \'%\',\'\')AS FLOAT64) AS Conversion_Rate, CAST(REPLACE(Total_Downloads, \',\', \'\') AS FLOAT64) AS Total_Downloads, CAST(REPLACE(Total_New_Download, \',\', \'\') AS FLOAT64) AS Total_New_Download, CAST(REPLACE(Total_Spends__meta_, \',\', \'\') AS FLOAT64) AS Total_Spends_meta, CAST(REPLACE(Cost_per_New_Download, \',\', \'\') AS FLOAT64) AS Cost_per_New_Download FROM MapleMonk.Zouk_App_Performance ;",
                     "transaction": true
                 }
             ) }}
