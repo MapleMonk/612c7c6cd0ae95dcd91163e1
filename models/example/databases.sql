7cbebf6c68e75064ddad3f9dc255a5d83d1c1614
MapleMonk-Dev <development@maplemonk.com>
2025-05-27 11:33:07 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE MapleMonk.Zouk_Fabric_QC_Update AS SELECT SAFE.PARSE_DATE(\'%m/%d/%Y\', CAST(Date AS STRING)) AS Date, CAST(Unit AS STRING) AS Unit, SAFE_CAST(Passed AS INT64) AS Passed, CAST(RM_Name AS STRING) AS RM_Name, SAFE_CAST(Rejected AS INT64) AS Rejected, CAST(QC_Person AS STRING) AS QC_Person, CAST(RM_Description AS STRING) AS RM_Description, CAST(FG_Description AS STRING) AS FG_Description, SAFE_CAST(Total_QC_Checked AS INT64) AS Total_QC_Checked FROM `MapleMonk.Zouk_Fabric_QC`;",
+                    "sql": "CREATE OR REPLACE TABLE MapleMonk.Zouk_Fabric_QC_Update AS SELECT SAFE.PARSE_DATE(\'%m/%d/%Y\', CAST(Date AS STRING)) AS Date, CAST(Unit AS STRING) AS Unit, SAFE_CAST(Passed AS INT64) AS Passed, CAST(RM_Name AS STRING) AS RM_Name, SAFE_CAST(Rejected AS INT64) AS Rejected, CAST(QC_Person AS STRING) AS QC_Person, CAST(RM_Description AS STRING) AS RM_Description, CAST(FG_Description AS STRING) AS FG_Description, SAFE_CAST(Total_QC_Checked AS INT64) AS Total_QC_Checked, CAST(Rejected_Reason AS STRING) AS Rejected_Reason FROM `MapleMonk.Fabric_QC`;",
                     "transaction": true
                 }
             ) }}
