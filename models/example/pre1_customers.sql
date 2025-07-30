22ecc356398736cfc521b99cb9732183354164f8
prudvi85 <prudvi_jagarlamudi@maplemonk.com>
2025-07-30 09:47:25 +0530

    model file has updated

--- models/example/pre1_customers.sql
+++ models/example/pre1_customers.sql
@@ -46,4 +46,20 @@
                 select * from sample_data
             )
             select * from final
+            {{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
             
