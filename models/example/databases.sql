b55c69c9ae05c261f82f792f16a757495b3cfb1a
MapleMonk-Dev <development@maplemonk.com>
2025-07-31 13:44:13 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE OR REPLACE TABLE MapleMonk.Zouk_Gifting_Inventory_Report AS WITH SKU_MASTER AS ( SELECT *, ROW_NUMBER() OVER (PARTITION BY Product_Code, Component_Product_Code ORDER BY Updated DESC) AS rn FROM Maplemonk.Unicommerce_zouk_uc_get_product_master ), FILTERED_SKUS AS ( SELECT UPPER(Product_Code) AS Parent_SKU, UPPER(Component_Product_Code) AS Child_SKU, UPPER(Name) AS Product_Name, UPPER(Color) AS Print, UPPER(Collection) AS Collection, UPPER(Product_Type) AS Product_Type, UPPER(Category_Code) AS Category_Code, UPPER(Category_Name) AS Category_Name FROM SKU_MASTER WHERE rn = 1 AND lower(collection) like \'gifting%\' ), INVENTORY AS ( SELECT UPPER(SKU) AS SKU_ID, SUM(Available_Inventory) AS Inventory_Qty FROM Maplemonk.ZOUK_INVENTORY_FACT_ITEMS WHERE DATA_FETCH_DATE = (SELECT MAX(DATA_FETCH_DATE) FROM Maplemonk.ZOUK_INVENTORY_FACT_ITEMS) GROUP BY SKU ) , CHILD_INVENTORY AS ( SELECT f.*, COALESCE(i.Inventory_Qty, 0) AS Child_Inventory FROM FILTERED_SKUS f LEFT JOIN INVENTORY i ON f.Child_SKU = i.SKU_ID ), PARENT_COMBO AS ( SELECT Parent_SKU, MIN(c.Child_Inventory) AS Parent_SKU_Inventory FROM CHILD_INVENTORY c GROUP BY Parent_SKU ), RANKED_COMBO AS ( SELECT c.*, ROW_NUMBER() OVER (PARTITION BY c.Parent_SKU ORDER BY c.Child_SKU) AS rn, p.Parent_SKU_Inventory FROM CHILD_INVENTORY c LEFT JOIN PARENT_COMBO p ON c.Parent_SKU = p.Parent_SKU ) SELECT Parent_SKU, Child_SKU, Product_Name, Print, Collection, Product_Type, Category_Code, Category_Name, Child_Inventory, CASE WHEN rn = 1 THEN Parent_SKU_Inventory ELSE 0 END AS Parent_SKU_Inventory FROM RANKED_COMBO ORDER BY Parent_SKU, Child_SKU ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
