9021fc5c4ce3ac75081e604be53561ddc8d18032
MapleMonk-Dev <development@maplemonk.com>
2025-08-21 13:21:55 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE OR REPLACE TABLE MapleMonk.zouk_production_sales_report AS SELECT commonsku AS SKU, PRODUCT_NAME_FINAL, PRODUCT_CATEGORY, COLLECTION, PRINT, PRODUCT_TYPE, SUM(CASE WHEN order_date = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) THEN QUANTITY ELSE 0 END) AS Quantity_Sold_Last, SUM(CASE WHEN order_date BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY) AND DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) THEN QUANTITY ELSE 0 END) AS Quantity_Sold_30, SUM(CASE WHEN order_date BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY) AND DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) THEN QUANTITY ELSE 0 END) AS Quantity_Sold_90 FROM `MapleMonk.zouk_sales_consolidated` GROUP BY 1,2,3,4,5,6",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
