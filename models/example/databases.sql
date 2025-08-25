28b3c75a6c08256c2c9e5b6a0268ff53f829f711
MapleMonk-Dev <development@maplemonk.com>
2025-08-25 10:48:37 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table maplemonk.daily_inventory as select skucode sku ,facility ,inventory ,b.name ,category ,sub_Category ,size ,style_no ,image_link ,selling_price ,mrp from MAPLEMONK.Unicommerce_Unicommerce_get_inventory_snapshot a left join (select * from (select sku , title name , category , sub_category , size , style_no , image_link , Selling_Price , mrp , row_number() over (partition by replace(sku,\' \',\'\') order by 1) rw from maplemonk.mapping_sku_master) where rw = 1 ) b on a.skucode = b.sku ;",
+                    "sql": "create or replace table maplemonk.daily_inventory as select skucode sku ,facility ,inventory ,b.name ,category ,sub_Category ,size ,style_no ,image_link ,selling_price ,mrp from MAPLEMONK.Unicommerce_Unicommerce_get_inventory_snapshot a left join ( select * from (select sku , title name , category , sub_category , size , style_no , image_link , Selling_Price , mrp , row_number() over (partition by replace(sku,\' \',\'\') order by 1) rw from maplemonk.mapping_sku_master ) where rw = 1 ) b on trim(lower(a.skucode)) = trim(lower(b.sku));",
                     "transaction": true
                 }
             ) }}
