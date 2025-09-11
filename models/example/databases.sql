6d894cfef5abf15280508f4c42679c06539d4d0d
MapleMonk-Dev <development@maplemonk.com>
2025-09-11 11:39:39 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table eggozdb.maplemonk.beat_tagging as select distributor_name,dealer_name,rr.code,rr.onboarding_status,sp.management_status, cu.name, case when sp.management_status =\'DSR\' then cu.name end DSR, case when sp.management_status =\'Sales Officer\' then cu.name end SO,rb.beat_area, rr.beat_number,rr.area_classification from eggozdb.maplemonk.my_sql_tertiary_retailer_retailer rr left join my_sql_retailer_retailerbeat rb on rb.id = rr.retailerBeat_id left join my_sql_saleschain_salespersonprofile sp on sp.id= rr.salesPersonProfile_id left join my_sql_custom_auth_user cu on sp.user_id = cu.id ;",
+                    "sql": "reate or replace table eggozdb.maplemonk.beat_tagging as select distributor_name,dealer_name,rr.code,rr.retailer_type,rr.category, rr.onboarding_status, dcu.name as dsr_name, scu.name as sr_name, socu.name as so_name, rb.beat_area, rr.beat_number,rr.area_classification from eggozdb.maplemonk.my_sql_tertiary_retailer_retailer rr left join my_sql_retailer_retailerbeat rb on rb.id = rr.retailerBeat_id left join my_sql_saleschain_salespersonprofile dsr on dsr.id= rb.DEALER_SALESREPRESENTATIVE_ID left join my_sql_saleschain_salespersonprofile sr on sr.id= rb.SALESREPRESENTATIVE_ID left join my_sql_saleschain_salespersonprofile so on so.id= rb.SALESOFFICER_ID left join my_sql_custom_auth_user dcu on dsr.user_id = dcu.id left join my_sql_custom_auth_user scu on sr.user_id = scu.id left join my_sql_custom_auth_user socu on so.user_id = socu.id ;",
                     "transaction": true
                 }
             ) }}
