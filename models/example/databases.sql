a55f97d6209179c3430f30bde1a167389f06db38
MapleMonk-Dev <development@maplemonk.com>
2025-09-16 10:32:01 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table maplemonk.HealthyMaster_zepto_fact_items_intermediate as select EAN, concat(EAN,sku_number,city,date,selling_price) as order_item, CAST(MRP AS FLOAT64) AS MRP, CITY, CAST(Date AS timestamp) as Order_Date, SKU_NAME AS Product_name, Brand_Name as Brand, sku_number, sku_category, cast(selling_price as float64) as selling_price, manufacturer_id, SKU_Sub_Category as sub_category, cast(Gross_Selling_Value as float64) as Gross_Selling_Value, cast(Sales__Qty____Units as int64) as quantity, cast(Gross_Merchandise_Value as float64) as Gross_Merchandise_Value from `MAPLEMONK.zepto_HM_sales` ; create or replace table maplemonk.HealthyMaster_zepto_fact_items as select z.* FROM maplemonk.HealthyMaster_zepto_fact_items_intermediate z;",
+                    "sql": "create or replace table maplemonk.HealthyMaster_zepto_sales_fact_items_intermediate as select EAN, concat(EAN,sku_number,city,date,Gross_Merchandise_Value) as order_item, CAST(MRP AS FLOAT64) AS MRP, CITY, CAST(Date AS timestamp) as Order_Date, SKU_NAME AS Product_name, Brand_Name as Brand, sku_number, sku_category, cast(Gross_Merchandise_Value as float64) as selling_price, manufacturer_id, SKU_Sub_Category as sub_category, cast(Gross_Merchandise_Value as float64) as Gross_Selling_Value, cast(Sales__Qty____Units as int64) as quantity, cast(Gross_Merchandise_Value as float64) as Gross_Merchandise_Value from `MAPLEMONK.zepto_HM_sales` ; create or replace table maplemonk.HealthyMaster_zepto_sales_fact_items as select z.* FROM maplemonk.HealthyMaster_zepto_sales_fact_items_intermediate z;",
                     "transaction": true
                 }
             ) }}
