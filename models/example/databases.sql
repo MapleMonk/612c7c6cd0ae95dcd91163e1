cf07b856c0b31cddffd833036f34d2ba7b092d9a
MapleMonk-Dev <development@maplemonk.com>
2025-10-07 20:16:49 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table maplemonk.symphony_wh_funnel_metrics_overall as select cast(format_date(\'%Y-%m-%d\', parse_date(\'%Y%m%d\', gaf.date)) as date) as date, cast(newUsers as int64) NewUsers, cast(sessions as int64) sessions, cast(checkouts as int64) checkouts, cast(addToCarts as int64) addtocarts, cast(totalUsers as int64) totalUsers, cast(transactions as int64) transactions, cast(engagedSessions as int64) engagedSessions, cast(screenPageViews as int64) screenPageViews, s.total_sales, s.total_orders, s.total_tax, gap.product_views from `Maplemonk.Symphony_GA4_funnel_overall_funnel_by_date` gaf left join (select date, sum(total_sales) total_sales, sum(total_orders) total_orders, sum(total_tax) total_tax from maplemonk.symphony_wh_sales_cost_source where upper(marketplace) = \'WEBSITE IN\' group by 1 ) s on s.date = cast(format_date(\'%Y-%m-%d\', parse_date(\'%Y%m%d\', gaf.date)) as date) left join (select date, sum(itemsViewed) as product_views from maplemonk.ga4_products_funnel_metrics group by 1 ) gap on gap.date = gaf.date;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
