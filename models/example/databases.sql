d0364347cef90bf6aca97a7c206d8cdd5b370808
MapleMonk-Dev <development@maplemonk.com>
2025-10-09 05:57:32 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE issues_factory_sorted AS SELECT CASE WHEN LEFT(EVIDENCE, 6) = \'https:\' THEN EVIDENCE ELSE \'https:\' || EVIDENCE END AS EVIDENCE_WITH_HTTPS,* FROM issues_factory_sorted;",
+                    "sql": "CREATE OR REPLACE TABLE issues_factory_sorted AS SELECT *,CASE WHEN LEFT(EVIDENCE, 6) = \'https:\' THEN EVIDENCE ELSE \'https:\' || EVIDENCE END AS EVIDENCE_WITH_HTTPS, FROM issues_factory_sorted;",
                     "transaction": true
                 }
             ) }}
