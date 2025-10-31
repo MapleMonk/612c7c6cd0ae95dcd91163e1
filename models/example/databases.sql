9d60cbc46151cdc3627b9b82f01e192535aa599a
MapleMonk-Dev <development@maplemonk.com>
2025-10-31 07:13:59 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "SELECT * FROM my_sql_procurement_qccomplaint LEFT JOIN my_sql_procurement_batchmodel ON my_sql_procurement_qccomplaint.BATCH_id = my_sql_procurement_batchmodel.id LEFT JOIN my_sql_procurement_procurement ON my_sql_procurement_batchmodel.procurement_id = my_sql_procurement_procurement.id LEFT JOIN my_sql_farmer_farm ON my_sql_procurement_procurement.farm_id = my_sql_farmer_farm.id;",
+                    "sql": "Create or replace table eggozdb.maplemonk.Customer_dashboard as SELECT QC.complaint_date,QC.complaint_type,QC.purchase_from,Qc.complaint_source,QC.complaint_body,Qc.complaint_resolution,QC.resolution_status,Qc.customer_location,Qc.customer_name, QC.batch_id, BM.ID as Batchlink,BM.DATE,bm.procurement_id, PP.id as procuremntIDlink ,pp.farm_id, FF.id as farmidlink ,FF.FARM_NAME FROM my_sql_procurement_qccomplaint as QC LEFT JOIN my_sql_procurement_batchmodel as BM ON QC.BATCH_id = BM.id LEFT JOIN my_sql_procurement_procurement as PP ON BM.procurement_id = PP.id LEFT JOIN my_sql_farmer_farm As FF ON PP.farm_id = FF.id;",
                     "transaction": true
                 }
             ) }}
