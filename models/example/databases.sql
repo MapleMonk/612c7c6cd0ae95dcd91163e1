8893ca8cdf52420319446a925aeffba3acf067f1
MapleMonk-Dev <development@maplemonk.com>
2025-11-03 10:51:43 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE issues_factory_sorted AS SELECT CONVERT_TIMEZONE(\'America/Los_Angeles\', \'Asia/Kolkata\', TO_TIMESTAMP_LTZ(CREATED_AT / 1000)) AS Created_At_date, CONVERT_TIMEZONE(\'America/Los_Angeles\', \'Asia/Kolkata\', TO_TIMESTAMP_LTZ(LAST_UPDATED_AT / 1000)) AS Updated_at_date, CASE WHEN LEFT(EVIDENCE, 6) = \'https:\' THEN EVIDENCE ELSE \'https:\' || EVIDENCE END AS EVIDENCE_WITH_HTTPS, t.* FROM issues_factory t;",
+                    "sql": "CREATE OR REPLACE TABLE issues_factory_sorted AS SELECT CONVERT_TIMEZONE(\'America/Los_Angeles\', \'Asia/Kolkata\', TO_TIMESTAMP_LTZ(CREATED_AT / 1000)) AS Created_At_date, CONVERT_TIMEZONE(\'America/Los_Angeles\', \'Asia/Kolkata\', TO_TIMESTAMP_LTZ(LAST_UPDATED_AT / 1000)) AS Updated_at_date, CASE WHEN LEFT(EVIDENCE, 6) = \'https:\' THEN EVIDENCE ELSE \'https:\' || EVIDENCE END AS EVIDENCE_WITH_HTTPS, t.* FROM factory_issues__factory t;",
                     "transaction": true
                 }
             ) }}
