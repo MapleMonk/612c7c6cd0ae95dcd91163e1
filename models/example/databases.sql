66e0b5c8abe1adec498892bf71a9cb184411c068
MapleMonk-Dev <development@maplemonk.com>
2025-11-12 05:56:23 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table snitch_db.maplemonk.inventory_delta as ( select trim(upper(REVERSE(SUBSTRING(REVERSE(\"Item SkuCode\"), CHARINDEX(\'-\', REVERSE(\"Item SkuCode\")) + 1, LEN(\"Item SkuCode\"))))) AS sku_group, ifnull(sum(case when date = current_date then inventory end),0) as current_date_inv, ifnull(sum(case when date = current_date-1 then inventory end),0) as last_date_inv, ifnull(sum(case when date = current_date then inventory end),0) - ifnull(sum(case when date = current_date-1 then inventory end),0) as inv_delta from snitch_db.maplemonk.snitch_final_inventory_wh2 WHERE facility in (\'SAPL-WH2\',\'SAPL-WH1\',\'SAPL-NORTH-TAURU\') group by 1 )",
+                    "sql": "create or replace table snitch_db.maplemonk.inventory_delta as ( with base as ( select trim(upper(REVERSE(SUBSTRING(REVERSE(\"Item SkuCode\"), CHARINDEX(\'-\', REVERSE(\"Item SkuCode\")) + 1, LEN(\"Item SkuCode\"))))) AS sku_group, ifnull(sum(case when date = current_date then inventory end),0) as current_date_inv, ifnull(sum(case when date = current_date-1 then inventory end),0) as last_date_inv, ifnull(sum(case when date = current_date then inventory end),0) - ifnull(sum(case when date = current_date-1 then inventory end),0) as inv_delta from snitch_db.maplemonk.snitch_final_inventory_wh2 WHERE facility in (\'SAPL-WH2\',\'SAPL-WH1\',\'SAPL-NORTH-TAURU\') group by 1 ) select base.*, p.current_slashed_price, p.price from base left join snitch_db.maplemonk.availability_master_v2 as p on base.sku_group=p.sku_group )",
                     "transaction": true
                 }
             ) }}
