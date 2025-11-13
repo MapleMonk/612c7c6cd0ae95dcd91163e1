795bddb8c5edeafdceee0eaf3d9848f523243d9a
MapleMonk-Dev <development@maplemonk.com>
2025-11-13 15:54:53 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "",
+                    "sql": "CREATE OR REPLACE TABLE maplemonk.MANIACLIFE_ASP_BR_TRAFFIC_FACT_ITEMS AS select cast(DATAENDTIME as date) Date ,\'AMAZON\' Marketplace ,parentasin ASIN ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(trafficbyasin,\'$.browserPageViews\') as float64),0)) Browser_Page_Views ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(trafficbyasin,\'$.browserSessions\') as float64),0)) Browser_Sessions ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(trafficbyasin,\'$.buyBoxPercentage\') as float64),0)) BuyBox_Percentage ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(trafficbyasin,\'$.mobileAppPageViews\') as float64),0)) MobileApp_Page_Views ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(trafficbyasin,\'$.mobileAppSessions\') as float64),0)) MobileApp_Sessions ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(trafficbyasin,\'$.pageViews\') as float64),0)) Page_Views ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(trafficbyasin,\'$.sessions\') as float64),0)) Sessions ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(salesbyasin,\'$.unitsOrdered\') as float64),0)) SC_UnitsOrdered ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(salesbyasin,\'$.totalOrderItems\') as float64),0)) SC_ItemsOrdered ,sum(ifnull(safe_Cast(JSON_EXTRACT_SCALAR(JSON_EXTRACT(salesbyasin,\'$.orderedProductSales\'),\'$.amount\') as float64),0)) SC_Sales from maplemonk.Maniaclife_ABR_GET_SALES_AND_TRAFFIC_REPORT_ASIN group by cast(DATAENDTIME as date), Marketplace, parentasin ;",
                     "transaction": true
                 }
             ) }}
