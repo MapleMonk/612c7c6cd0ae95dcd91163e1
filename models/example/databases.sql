449878f0fc128d8661728a8dd7516bf8d7a18412
MapleMonk-Dev <development@maplemonk.com>
2025-11-27 22:15:17 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "drop table if exists public.Tuco_Kids_flipkart_seller_pla_pca_fact_items; create table public.Tuco_Kids_flipkart_seller_pla_pca_fact_items as select ACCOUNT_ID ,\"campaign id\" as CAMPAIGN_NAME ,\"campaign id\" as CAMPAIGN_ID ,\"campaign type\" as Campaign_Type ,\"budgeting type\" as Campaign_Budgeting_Type ,\"campaign budget\" as CAMPAIGN_BUDGET ,\"campaign status\" as CAMPAIGN_STATUS ,to_date(start_time, \'YYYY-MM-DD\')::date as DATE ,\'FLIPKART\' Channel ,\'FLIPKART SELLER PLA\' Account ,cast(clicks as bigint) Clicks ,cast(\"Ad Spend\" as DOUBLE PRECISION) Spend ,cast(views as bigint) Views ,ROI ,cast(\"total converted units\" as bigint) Total_Converted_units ,cast(\"total revenue (rs.)\" as DOUBLE PRECISION) Total_Revenue from public.Flipkart_tuco_kids_seller_portal_pla UNION ALL select ACCOUNT_ID ,CAMPAIGN_NAME ,CAMPAIGN_ID ,cast(null as varchar) Campaign_Type ,\"campaign budget type\" as Campaign_Budget_Type ,Campaign_Budget CAMPAIGN_BUDGET ,CAMPAIGN_STATUS ,to_date(start_time, \'YYYY-MM-DD\')::date as DATE ,\'FLIPKART\' Channel ,\'FLIPKART SELLER PCA\' Account ,cast(clicks as bigint) Clicks ,cast(campaign_spend as DOUBLE PRECISION) Spend ,cast(views as bigint) Views ,\"direct roi\" as roi ,(cast(\"direct units\" as bigint) + cast(\"indirect units\" as bigint)) Total_Converted_units ,(cast(\"direct revenue\" as DOUBLE PRECISION) + cast(\"indirect units\" as DOUBLE PRECISION)) Total_Revenue from public.Flipkart_tuco_kids_seller_portal_pca ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select database, schema, "table" from SVV_TABLE_INFO limit 1
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
