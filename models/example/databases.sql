382b4b181e66c019ec129b26536662951c8b577c
MapleMonk-Dev <development@maplemonk.com>
2026-01-08 04:48:21 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "Create or replace table MapleMonk.Inventory_WIP_Only AS SELECT DATETIME_TRUNC(`DATA_FETCH_DATE`, DAY) AS `DATA_FETCH_DATE`, `Category` AS `Category`, SKU, Product_Final_Name, COLOR, SUB_CATEGORY, COLLECTION, PRINT, PRODUCT_TYPE, BAU_ONLINE,BRAND, sum(ifnull(AVAILABLE_INVENTORY, 0)) AS Total_Inventory, sum(ifnull(WIP_INVENTORY, 0)) AS WIP_Inventory FROM `MapleMonk`.`ZOUK_INVENTORY_FACT_ITEMS` GROUP BY 1,2,3,4,5,6,7,8,9,10,11",
+                    "sql": "Create or replace table MapleMonk.Inventory_WIP_Only AS SELECT DATETIME_TRUNC(`DATA_FETCH_DATE`, DAY) AS `DATA_FETCH_DATE`, `Product_Category` AS `Category`, SKU, Product_Name_Final, COLOR, SUB_CATEGORY, COLLECTION, PRINT, PRODUCT_TYPE, BAU_ONLINE,BRAND, sum(ifnull(AVAILABLE_INVENTORY, 0)) AS Total_Inventory, sum(ifnull(WIP_INVENTORY, 0)) AS WIP_Inventory FROM `MapleMonk`.`ZOUK_INVENTORY_FACT_ITEMS` GROUP BY 1,2,3,4,5,6,7,8,9,10,11",
                     "transaction": true
                 }
             ) }}
