05fb27f25d716feda81a0e32f186129f2a801a79
MapleMonk-Dev <development@maplemonk.com>
2026-01-29 07:31:14 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "ALTER SESSION SET TIMEZONE = \'Asia/Kolkata\'; CREATE OR REPLACE TABLE SNITCH_DB.MAPLEMONK.LIVE_INV_WAREHOUSE_offline_replen AS WITH data AS ( SELECT \"Item SkuCode\" AS sku, FACILITY, COALESCE(SUM(inventory), 0) AS qty FROM snitch_db.maplemonk.snitch_final_inventory_wh2 WHERE date = current_date AND FACILITY in (\'SAPL-WH2\',\'SAPL-WH1\',\'SAPL-NORTH-TAURU\') GROUP BY sku, FACILITY ) SELECT sku, facility, SUM(qty) as units_on_hand FROM data GROUP BY sku, facility ;",
+                    "sql": "ALTER SESSION SET TIMEZONE = \'Asia/Kolkata\'; CREATE OR REPLACE TABLE SNITCH_DB.MAPLEMONK.LIVE_INV_WAREHOUSE_offline_replen AS WITH data AS ( SELECT \"Item SkuCode\" AS sku, FACILITY, COALESCE(SUM(inventory), 0) AS qty FROM snitch_db.maplemonk.unicommerce_live_inventory WHERE FACILITY in (\'SAPL-WH2\',\'SAPL-WH1\',\'SAPL-NORTH-TAURU\') GROUP BY sku, FACILITY ) SELECT sku, facility, SUM(qty) as units_on_hand FROM data GROUP BY sku, facility ;",
                     "transaction": true
                 }
             ) }}
