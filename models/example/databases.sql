b44e79190ba46f84b63fb89ddec2540673887c26
MapleMonk-Dev <development@maplemonk.com>
2026-02-04 04:51:28 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "",
+                    "sql": "CREATE OR REPLACE TABLE MapleMonk.Zouk_FG_QC_Report AS SELECT SAFE_CAST(`Month` AS STRING) AS Month, SAFE.PARSE_DATE(\'%d-%b-%y\', `Date`) AS Date, SAFE_CAST(`Vendor_Name` AS STRING) AS Vendor_Name, SAFE_CAST(`Category` AS STRING) AS Category_Name, SAFE_CAST(`Qc_Pass` AS INT64) AS QC_Accepted, SAFE_CAST(`Qc_Damaged` AS INT64) AS QC_Rejected, FROM `MapleMonk.Zouk_wh_FG_QC` WHERE Date IS NOT NULL",
                     "transaction": true
                 }
             ) }}
