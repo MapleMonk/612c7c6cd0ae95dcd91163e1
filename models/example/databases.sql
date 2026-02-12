58c2af27d644f8a10696acfa8ca4141829057474
MapleMonk-Dev <development@maplemonk.com>
2026-02-12 03:53:48 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table maplemonk.samandmarshall_myntra_consolidated_advertisement_fact_items as select date(parse_date(\'%Y%m%d\',date)) Date, safe_cast(ctr as float64) CTR, safe_cast(cvr as float64) CVR, cast(clicks as int64) Clicks, cast(impressions as int64) Impressions, safe_cast(avg_cpc as float64) CPC, safe_cast(ad_spend as float64) Spend, safe_cast(roi_total as float64) Toal_ROI, account_id, adgroup_id, safe_cast(roi_direct as float64) roi_direct, campaign_id, campaign_name from maplemonk.myntra_sm_consolidated_daily;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
