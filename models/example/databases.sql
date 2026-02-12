25822a502deb1889bd5bde4f2938a7aa5ebd08cb
MapleMonk-Dev <development@maplemonk.com>
2026-02-12 03:58:41 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table maplemonk.samandmarshall_myntra_consolidated_advertisement_fact_items as select date(parse_date(\'%Y%m%d\',date)) Date, safe_cast(ctr as float64) CTR, safe_cast(cvr as float64) CVR, cast(clicks as int64) Clicks, cast(impressions as int64) Impressions, safe_cast(avg_cpc as float64) CPC, safe_cast(ad_spend as float64) Spend, safe_cast(roi_total as float64) Toal_ROI, account_id, adgroup_id, safe_cast(roi_direct as float64) roi_direct, campaign_id, campaign_name from maplemonk.myntra_sm_consolidated_daily;",
+                    "sql": "create or replace table maplemonk.samandmarshall_myntra_consolidated_advertisement_fact_items as select date(parse_date(\'%Y%m%d\',date)) Date, safe_cast(ctr as float64) CTR, safe_cast(cvr as float64) CVR, cast(clicks as int64) Clicks, cast(impressions as int64) Impressions, safe_cast(avg_cpc as float64) CPC, safe_cast(ad_spend as float64) Spend, safe_cast(roi_total as float64) Total_ROI, account_id, adgroup_id, adgroup_name, cast(indirect_revenue as float64) as indirect_revenue, cast(direct_revenue as float64) as direct_revenue, cast(total_revenue as float64) as total_revenue, cast(units_sold_total as int64) as conversions, adgroup_targetting_type, campaign_status, adgroup_status, safe_cast(roi_direct as float64) roi_direct, campaign_id, campaign_name from maplemonk.myntra_sm_consolidated_daily;",
                     "transaction": true
                 }
             ) }}
