7a19f5b6f28e2297233d9659921bc5bb85b988cd
MapleMonk-Dev <development@maplemonk.com>
2026-02-26 09:31:29 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "create or replace table maplemonk.saadaa_grn_cashflow_report as with base_data as ( select case when upper(g.po_ref_num) like \'%EFOB%\' then \'E-FOB\' when upper(g.po_ref_num) like \'%FOB%\' then \'FOB\' else \'Job\' end as PO_Type, g.grn_id, g.po_detail_id, g.grn_status, g.vendor_name, g.po_created_date, g.grn_created_date, g.grn_invoice_date, g.last_grn_date, DATE_ADD( cast(g.grn_created_date as date), INTERVAL GREATEST(45, IFNULL(safe_cast(regexp_extract(v.paymentTerm, r\'(\d+)\') as int64), 0)) DAY ) as payment_due_date, g.sku, (g.grn_detail_price * g.received_quantity) as grn_value, g.grn_detail_price, qc.QC_Fail_Quantity, qc.QC_Pass_Quantity from maplemonk.saadaa_grn_fact_items g LEFT JOIN `maplemonk.Easyecom_Saadaa_vendors` v on lower(trim(g.vendor_name)) = lower(trim(v.vendor_Name)) LEFT JOIN (SELECT PO, GRN, REPLACE(SKU,\'`\',\'\') SKU, SUM(QC_Fail_Quantity) QC_FAIL_QUANTITY, SUM(QC_Pass_Quantity) QC_PASS_QUANTITY FROM maplemonk.saadaa_easyecom_qc_report WHERE PO <> \'\' or GRN <> \'\' GROUP BY 1,2,3) qc on cast(qc.PO as string) = cast(g.po_id as string) and cast(qc.grn as string) = cast(g.grn_id as string) and lower(REPLACE(qc.sku,\'`\',\'\')) = lower(g.sku) ) select *, CASE WHEN EXTRACT(DAY FROM payment_due_date) <= 15 THEN DATE(EXTRACT(YEAR FROM payment_due_date), EXTRACT(MONTH FROM payment_due_date), 15) ELSE LAST_DAY(payment_due_date, MONTH) END as tentative_payment_date from base_data;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from maplemonk.INFORMATION_SCHEMA.TABLES
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
