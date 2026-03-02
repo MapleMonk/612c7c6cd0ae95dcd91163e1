a8deccdccf7e10878836ac24a5e1137d23ac8fa3
MapleMonk-Dev <development@maplemonk.com>
2026-03-02 10:01:24 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE shoot_type_monthly_summary AS WITH base AS ( SELECT TRY_TO_TIMESTAMP( NULLIF(NULLIF(TRIM(shoot_date), \'\'), \'null\'), \'DD-MM-YYYY HH24:MI:SS\' ) AS parsed_shoot_date, CASE WHEN TRIM(shoot_type) = \'\' THEN NULL WHEN LOWER(TRIM(shoot_type)) = \'null\' THEN NULL ELSE shoot_type END AS clean_shoot_type FROM pslj_final_new_pslj_table ) SELECT TO_VARCHAR(parsed_shoot_date, \'YYYY-MM\') AS shoot_month, COUNT(*) AS total_records, COUNT(clean_shoot_type) AS shoot_type_filled, ROUND( 100.0 * COUNT(clean_shoot_type) / NULLIF(COUNT(*), 0), 2 ) AS shoot_type_percentage FROM base WHERE parsed_shoot_date IS NOT NULL GROUP BY 1 ORDER BY 1;",
+                    "sql": "CREATE OR REPLACE TABLE shoot_type_monthly_summary AS WITH base AS ( SELECT TRY_TO_TIMESTAMP( NULLIF(NULLIF(TRIM(shoot_date), \'\'), \'null\'), \'DD-MM-YYYY HH24:MI:SS\' ) AS parsed_shoot_date, CASE WHEN TRIM(shoot_type) = \'\' THEN NULL WHEN LOWER(TRIM(shoot_type)) = \'null\' THEN NULL ELSE shoot_type END AS clean_shoot_type FROM pslj_final_new_pslj_table ) SELECT TO_CHAR(parsed_shoot_date, \'YYYY-MM-DD\') AS shoot_date, COUNT(*) AS total_records, COUNT(clean_shoot_type) AS shoot_type_filled, ROUND( 100.0 * COUNT(clean_shoot_type) / NULLIF(COUNT(*), 0), 2 ) AS shoot_type_percentage FROM base WHERE parsed_shoot_date IS NOT NULL AND parsed_shoot_date >= \'2026-01-01\' GROUP BY 1 ORDER BY 1;",
                     "transaction": true
                 }
             ) }}
