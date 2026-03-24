532e1415d309671ec5468e1396427c61ab1bb8c4
MapleMonk-Dev <development@maplemonk.com>
2026-03-24 08:35:31 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE MapleMonk.Eatanytime_Blinkit_Fact_items AS SELECT Order_ID as order_id, PARSE_DATE(\'%e %b %Y\', Order_Date) AS order_date, cast(MRP__Rs_ as float64) mrp, Customer_City as city, Item_ID as product_id, cast(replace(Quantity,\'.0\',\'\') as int64) quantity, Product_Name as product_name, bs.L0_Category as product_category, L1_Category as sub_category, L2_Category as sub_sub_category, Product_name as Product_name_final, cast(Selling_Price__Rs_ as float64) as Selling_Price_final FROM `maplemonk.Eatanytime_blinkit_sales` bs ;",
+                    "sql": "CREATE OR REPLACE TABLE Maplemonk.eatanytime_blinkit_Fact_Items AS SELECT CAST(S_No_ AS INT64) AS S_No, CAST(Item_ID AS STRING) AS Item_ID, CAST(HSN_Code AS STRING) AS HSN_Code, CAST(MRP__Rs_ AS FLOAT64) AS MRP, CAST(Order_Id AS STRING) AS Order_ID, CAST(Quantity AS INT64) AS Quantity, CAST(REPLACE(Total_Tax, \'-\', \'0\') AS FLOAT64) AS Total_Tax, cast(left(Order_Date,10) as date) AS Order_Date, CAST(Order_Status AS STRING) AS Order_Status, CAST(Product_Name AS STRING) AS Product_Name, CAST(Supply_City AS STRING) AS Supply_City, CAST(Customer_City AS STRING) AS Customer_City, CAST(Selling_Price__Rs_ AS FLOAT64) AS Selling_Price_Rs, CAST(Variant_Description AS STRING) AS Variant_Description, CAST(Total_Gross_Bill_Amount AS FLOAT64) AS Total_Gross_Bill_Amount, from maplemonk.eatanytime_blinkit_new_sales s ;",
                     "transaction": true
                 }
             ) }}
