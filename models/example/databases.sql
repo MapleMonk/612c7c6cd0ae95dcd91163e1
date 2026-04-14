a5e0a0a451da086e137a9c9400f8e9cd2abb1083
MapleMonk-Dev <development@maplemonk.com>
2026-04-14 04:34:30 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE MapleMonk.BabyGo_Blinkit_Fact_items AS SELECT distinct concat(mrp,\'-\',item_id,City_name,\'-\',city_id,\'-\',\'-\',CAST(qty_sold as float64),date) as order_id, cast(date as date) order_date, cast(mrp as float64) mrp, city_name as city, item_id as product_id, cast(replace(qty_sold,\'.0\',\'\') as int64) quantity, item_name as product_name FROM `maplemonk.BabyGo_Blinkit_sales_partner_biz` ;",
+                    "sql": "CREATE OR REPLACE TABLE MapleMonk.BabyGo_Blinkit_Fact_items AS SELECT distinct concat(mrp,\'-\',item_id,City_name,\'-\',city_id,\'-\',\'-\',CAST(qty_sold as float64),date) as order_id, cast(date as date) order_date, cast(mrp as float64) mrp, city_name as city, item_id as product_id, cast(replace(qty_sold,\'.0\',\'\') as int64) quantity, item_name as product_name, b.lot, b.category, b.style_id FROM `maplemonk.BabyGo_Blinkit_sales_partner_biz` a left join (select item_code, sku_number, lot, style_id, category from maplemonk.gs_qc_sku_mapping where platform = \'BLINKIT\' qualify row_number() over (partition by item_code order by sku_number) = 1 ) b on a.item_id = b.item_code ;",
                     "transaction": true
                 }
             ) }}
