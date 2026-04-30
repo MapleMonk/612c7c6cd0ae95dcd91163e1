5c6de87b9c3a542933f3c1440dc9661b722c803e
MapleMonk-Dev <development@maplemonk.com>
2026-04-30 11:17:08 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE `MAPLEMONK.SAADAA_order_funnel` AS WITH base_data AS ( SELECT OMS_order_status, FORMAT_DATE(\'%Y\', Order_Date) AS Year, FORMAT_DATE(\'%b\', Order_Date) AS Month_Name, FORMAT_DATE(\'%m\', Order_Date) AS Month_Num, reference_code, QUANTITY FROM `MAPLEMONK.SAADAA_sales_consolidated` ) SELECT OMS_order_status, Year, Month_Name, Month_Num, \'Order Qty\' AS Metric_Type, COUNT(DISTINCT reference_code) AS Value FROM base_data GROUP BY 1, 2, 3, 4, 5 UNION ALL SELECT OMS_order_status, Year, Month_Name, Month_Num, \'Item Qty\' AS Metric_Type, SUM(QUANTITY) AS Value FROM base_data GROUP BY 1, 2, 3, 4, 5;",
+                    "sql": "CREATE OR REPLACE TABLE `MAPLEMONK.SAADAA_order_funnel` AS WITH base_data AS ( SELECT OMS_order_status, FORMAT_DATE(\'%Y\', Order_Date) AS Year, FORMAT_DATE(\'%b\', Order_Date) AS Month_Name, FORMAT_DATE(\'%m\', Order_Date) AS Month_Num, DATE_TRUNC(Order_Date, MONTH) AS Month_Date, reference_code, QUANTITY FROM `MAPLEMONK.SAADAA_sales_consolidated` ) SELECT OMS_order_status, Year, Month_Name, Month_Num, Month_Date, \'Order Qty\' AS Metric_Type, COUNT(DISTINCT reference_code) AS Value FROM base_data GROUP BY 1, 2, 3, 4, 5, 6 UNION ALL SELECT OMS_order_status, Year, Month_Name, Month_Num, Month_Date, \'Item Qty\' AS Metric_Type, SUM(QUANTITY) AS Value FROM base_data GROUP BY 1, 2, 3, 4, 5, 6;",
                     "transaction": true
                 }
             ) }}
