36ba2532d49a3779396d84c67c51e7bfdcd6ca0f
MapleMonk-Dev <development@maplemonk.com>
2026-04-30 10:16:47 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table snitch_db.maplemonk.plus_spends_roas as with spends as ( select date, ifnull(sum(case when (lower(ad_name) like \'%plus%\' or lower(adset_name) like \'%plus%\' or (lower(campaign_name) like \'%plus%\') then spend end)::int,0) from snitch_db.maplemonk.marketing_consolidated_snitch group by 1 ), sales as ( select date as date, sum(gross_sales) as sales from snitch_db.maplemonk.horizontal_sales_categories a left join snitch_db.maplemonk.base_product b on a.sku_group = b.sku_group where b.l1_category = \'plus\' and type =\'Shopify\' group by 1 ) select a.*, b.sales, div0(sales,spend) as plus_roas from spends a left join sales b on a.date = b.date ;",
+                    "sql": "create or replace table snitch_db.maplemonk.plus_spends_roas as with spends as ( select date, ifnull(sum(case when lower(ad_name) like \'%plus%\' or lower(adset_name) like \'%plus%\' or lower(campaign_name) like \'%plus%\' then spend end)::int,0) as spend from snitch_db.maplemonk.marketing_consolidated_snitch group by 1 ), sales as ( select date as date, sum(gross_sales) as sales from snitch_db.maplemonk.horizontal_sales_categories a left join snitch_db.maplemonk.base_product b on a.sku_group = b.sku_group where b.l1_category = \'plus\' and type =\'Shopify\' group by 1 ) select a.*, b.sales, div0(sales,spend) as plus_roas from spends a left join sales b on a.date = b.date ;",
                     "transaction": true
                 }
             ) }}
