69c95697c6da7b1c395a5a5b952b0028adcb453d
MapleMonk-Dev <development@maplemonk.com>
2026-05-27 05:23:14 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "Create or replace table prolicious-wh.maplemonk.prolicious_parent_child_sku_mapping_fact_items as SELECT PARENT_SKU as Parent_SKU, TRIM(SPLIT(component, \'&\')[OFFSET(0)]) AS Child_sku, CAST(TRIM(SPLIT(component, \'&\')[OFFSET(1)]) AS INT64) AS Quantity FROM maplemonk.Prolicious_SKU_MAPPING_PARENT_CHILD, UNNEST([ Component1_SKU, Component2_SKU, Component3_SKU, Component4_SKU, Component5_SKU, Component6_SKU, Component7_SKU, Component8_SKU, Component9_SKU, Component10_SKU, Component11_SKU ]) AS component WHERE component IS NOT NULL ;",
+                    "sql": "Create or replace table prolicious-wh.maplemonk.prolicious_parent_child_sku_mapping_fact_items as SELECT PARENT_SKU as Parent_SKU, TRIM(SPLIT(component, \'&\')[OFFSET(0)]) AS Child_sku, CAST(TRIM(SPLIT(component, \'&\')[OFFSET(1)]) AS INT64) AS Child_quantity FROM maplemonk.Prolicious_SKU_MAPPING_PARENT_CHILD, UNNEST([ Component1_SKU, Component2_SKU, Component3_SKU, Component4_SKU, Component5_SKU, Component6_SKU, Component7_SKU, Component8_SKU, Component9_SKU, Component10_SKU, Component11_SKU ]) AS component WHERE component IS NOT NULL ;",
                     "transaction": true
                 }
             ) }}
