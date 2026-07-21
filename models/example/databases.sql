49e06028b1d7a6110a14d32b2e65f2b174513ec3
MapleMonk-Dev <development@maplemonk.com>
2026-07-21 11:18:02 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.store_priority_tier AS WITH latest_month AS ( SELECT branch_code, target_month, mtd_target, mtd_achieved FROM snitch_db.maplemonk.mtd_target_clean QUALIFY ROW_NUMBER() OVER (PARTITION BY branch_code ORDER BY target_month DESC) = 1 ), tiered AS ( SELECT *, NTILE(3) OVER (ORDER BY mtd_target DESC) AS target_tertile FROM latest_month WHERE mtd_target IS NOT NULL ) SELECT branch_code, target_month AS current_month, mtd_target AS current_month_target, mtd_achieved, CASE WHEN target_tertile = 1 THEN \'High Priority\' WHEN target_tertile = 2 THEN \'Medium Priority\' ELSE \'Low Priority\' END AS store_priority_tier FROM tiered;",
+                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.store_priority_tier AS WITH latest_month AS ( SELECT branch_code, target_month, cumulative_target_mtd FROM snitch_db.maplemonk.mtd_target_clean QUALIFY ROW_NUMBER() OVER (PARTITION BY branch_code ORDER BY target_month DESC) = 1 ), tiered AS ( SELECT *, NTILE(3) OVER (ORDER BY cumulative_target_mtd DESC) AS target_tertile FROM latest_month WHERE cumulative_target_mtd IS NOT NULL ) SELECT branch_code, target_month AS current_month, cumulative_target_mtd AS current_month_target, CASE WHEN target_tertile = 1 THEN \'High Priority\' WHEN target_tertile = 2 THEN \'Medium Priority\' ELSE \'Low Priority\' END AS store_priority_tier FROM tiered;",
                     "transaction": true
                 }
             ) }}
