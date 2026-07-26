623a1650b84975bff40a3b3eafb84a7a1665bb19
MapleMonk-Dev <development@maplemonk.com>
2026-07-26 14:23:41 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "Create Or Replace Table SLEEPYCAT_DB.MAPLEMONK.SLEEPYCAT_DB_SWIGGY_FACT_ITEMS as select ordered_date::date as Order_Date, item_code, City, units_sold, ifnull(base_mrp,0) * ifnull(units_sold,0) as MRP, ifnull(GMV,0) as GMV, ifnull(invoice_value,0)*ifnull(units_sold,0) as Final_Selling_price, qc.\"Master SKU Code\" as commonsku, s.name, s.category, area_name, from sleepycat_db.maplemonk.sleepycat_db_sales z LEFT JOIN ( SELECT * FROM sleepycat_db.maplemonk.gs_mapping_qc_sku_mapping WHERE lower(platform) = \'swiggy\' QUALIFY ROW_NUMBER() OVER (PARTITION BY priority ORDER BY 1) = 1 ) qc ON upper(qc.priority) = (z.item_code) LEFT JOIN ( select * from sleepycat_db.maplemonk.final_sku_master qualify row_number() over(partition by lower(trim(skucode)) order by 1) = 1 )s on lower(trim(qc.\"Master SKU Code\")) = lower(trim(s.skucode)) left join ( select sku, date, replace(\"Invoice value\",\',\',\'\')::float invoice_value, row_number() over (partition by sku, date order by 1) rw from sleepycat_db.maplemonk.gs_qc_taxable where lower(portal) = \'swiggy\' qualify row_number() over (partition by lower(sku), date order by 1) = 1 ) tv on lower(qc.\"Master SKU Code\") = lower(tv.sku) and date_trunc(month,z.ordered_date::date) = tv.date ;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from SLEEPYCAT_DB.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
