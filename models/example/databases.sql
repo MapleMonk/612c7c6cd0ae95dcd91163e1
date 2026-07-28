e7be0edc8cb9bb4c93840683b849dd770747ba99
MapleMonk-Dev <development@maplemonk.com>
2026-07-28 09:58:01 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.store_zone_map AS SELECT branch_code, zone FROM ( SELECT branch_code::VARCHAR AS branch_code, UPPER(zone) AS zone, ROW_NUMBER() OVER (PARTITION BY branch_code::VARCHAR ORDER BY zone) AS rn FROM snitch_db.maplemonk.store_category_delta WHERE UPPER(zone) IN (\'NORTH\', \'SOUTH\') ) WHERE rn = 1;",
+                    "sql": "CREATE OR REPLACE TABLE snitch_db.maplemonk.store_zone_map AS SELECT branch_code, store_name, zone, cluster_name FROM ( SELECT s.branch_code::VARCHAR AS branch_code, s.branch_name::VARCHAR AS store_name, UPPER(s.zone) AS zone, m.cluster::VARCHAR AS cluster_name, ROW_NUMBER() OVER ( PARTITION BY s.branch_code::VARCHAR ORDER BY UPPER(s.zone) ) AS rn FROM snitch_db.maplemonk.store_category_delta s LEFT JOIN snitch_db.maplemonk.offline_master m ON s.branch_code::VARCHAR = m.branch_code::VARCHAR WHERE UPPER(s.zone) IN (\'NORTH\', \'SOUTH\') ) WHERE rn = 1;",
                     "transaction": true
                 }
             ) }}
