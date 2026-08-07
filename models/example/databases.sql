ae39b3a86776cfdc0d9443eeb60ae7acbbc54656
MapleMonk-Dev <development@maplemonk.com>
2026-08-07 08:44:03 +0000

    model file has updated

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,13 +1,13 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "create or replace table snitch_db.maplemonk.discount_code_usage_metrics as select order_timestamp::date as order_date, discount_code, sum(gross_sales) as gross_sales, sum(quantity) as gross_quantity, sum(case when lower(discount_code) is not null and lower(discount_code) like \'%rms%\' then 0 else discount end) as discount, count(distinct order_name) as gross_orders, sum(gross_sales)/count(distinct order_name) as gross_aov from snitch_db.maplemonk.fact_items_snitch where lower(ifnull(discount_code,\'n\')) not like \'%eco%\' and lower(ifnull(discount_code,\'n\')) not like \'%influ%\' and order_name not in (\'2431093\',\'2422140\',\'2425364\',\'2430652\',\'2422237\',\'2420623\',\'2429832\',\'2422378\',\'2428311\',\'2429064\',\'2428204\',\'2421343\',\'2431206\',\'2430491\',\'2426682\',\'2426487\',\'2426458\',\'2423575\',\'2422431\',\'2423612\',\'2426625\',\'2428117\',\'2426894\',\'2425461\',\'2426570\',\'2423455\',\'2430777\',\'2426009\',\'2428245\',\'2427269\',\'2430946\',\'2425821\',\'2429986\',\'2429085\',\'2422047\',\'2430789\',\'2420219\',\'2428341\',\'2430444\',\'2426866\',\'2431230\',\'2425839\',\'2430980\',\'2427048\',\'2430597\',\'2420499\',\'2431050\',\'2420271\',\'2426684\',\'2428747\',\'2423523\',\'2431171\',\'2430830\',\'2425325\',\'2428414\',\'2429054\',\'2423596\') and tags not in (\'FLITS_LOGICERP\') and lower(tags) not like \'%creator%\' and tags not like \'%SnitchCreators%\' group by 1,2 ;",
+                    "sql": "create or replace table snitch_db.maplemonk.discount_code_usage_metrics as ( with main as ( select *, case when lower(ifnull(discount_code,\'others\')) like \'%eco%\' or lower(ifnull(discount_code,\'others\')) like \'%influ%\' or lower(ifnull(discount_code,\'others\')) like \'%rms%\' or tags = \'FLITS_LOGICERP\' or lower(ifnull(discount_code,\'others\')) like \'%creator%\' then 1 else 0 end as filters from snitch_db.maplemonk.fact_items_snitch where order_status = \'Shopify_Processed\' ), agg as ( select order_timestamp::date as order_date, discount_code, sum(gross_sales) as gross_sales, sum(quantity) as gross_quantity, sum(case when lower(discount_code) is not null and lower(discount_code) like \'%rms%\' then 0 else discount end) as discount, count(distinct order_name) as gross_orders, sum(gross_sales)/count(distinct order_name) as gross_aov from main where filters = 0 group by 1,2 ) select *, sum(gross_sales) over (partition by order_date) as day_gross_sales, gross_sales / nullif(sum(gross_sales) over (partition by order_date), 0) as sales_share from agg order by order_date, sales_share desc );",
                     "transaction": true
                 }
             ) }}
             with sample_data as (
 
-                select * from snitch_db.information_schema.databases
+                select * from SNITCH_DB.information_schema.databases
             ),
             
             final as (
