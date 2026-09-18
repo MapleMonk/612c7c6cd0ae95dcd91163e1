7d3bc582475224cd7d4b8196e8032a5c1dec3c2e
MapleMonk-Dev <development@maplemonk.com>
2026-09-18 12:34:15 +0000

    model file has updated

--- models/example/Postgres_city.sql
+++ models/example/Postgres_city.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "SELECT u.name, u.phone_number, MIN(o.delivered_at) AS first_order_date, w.created_at AS wallet_recharge_date FROM postgres_users u LEFT JOIN postgres_orders o ON u.id = o.user_id LEFT JOIN postgres_wallet w ON u.id = w.user_id WHERE u.is_deleted = FALSE GROUP BY u.name, u.phone_number, w.created_at;",
+                    "sql": "CREATE OR REPLACE TABLE eggozdb.maplemonk.postgre_total_customer AS SELECT u.name, u.phone_number, MIN(o.delivered_at) AS first_order_date, w.created_at AS wallet_recharge_date FROM postgres_users u LEFT JOIN postgres_orders o ON u.id = o.user_id LEFT JOIN postgres_wallet w ON u.id = w.user_id WHERE u.is_deleted = FALSE GROUP BY u.name, u.phone_number, w.created_at;",
                     "transaction": true
                 }
             ) }}
